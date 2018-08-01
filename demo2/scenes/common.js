94ede1c953b4ce566dac1f2140bffe3d2d75d2ad
Starykh Aleksey <astarikh@mail.ru>
2018-08-01 17:02:46 +0300

    shining fix, meteor rain

--- demo2/scenes/common.js
+++ demo2/scenes/common.js
@@ -5,25 +5,41 @@ class StarsLayer {
                 count: 20,
                 opacity: 0.5,
                 speed: 0.1,
-                direction: new V2(-1, 0)
+                direction: new V2(-1, 0),
             },
-            level: 0
+            level: 0,
+            addShiningStars: false
         }, options);
 
         this.stars = [];
 
         for(let i = 0; i < this.starsConfigs.count; i++){
+
+            let starProps = {
+                position: new V2(getRandomInt(0, this.scene.viewport.x), getRandomInt(0, this.scene.viewport.y)),
+                opacity: this.starsConfigs.opacity,
+                speed: this.starsConfigs.speed,
+                direction: this.starsConfigs.direction,
+            };
+
             this.stars.push(this.scene.addGo(
-                new Star({
-                    position: new V2(getRandomInt(0, this.scene.viewport.x), getRandomInt(0, this.scene.viewport.y)),
-                    opacity: this.starsConfigs.opacity,
-                    speed: this.starsConfigs.speed,
-                    direction: this.starsConfigs.direction
-                }), this.level
+                (this.addShiningStars && getRandomInt(0, 300) < 10)? new ShiningStar(starProps) : new Star(starProps)
+                , this.level
             ));
         }
+
+        // this.scene.addGo(
+        //     new ShiningStar({
+        //         position: new V2(getRandomInt(0, this.scene.viewport.x), getRandomInt(0, this.scene.viewport.y)),
+        //         opacity: this.starsConfigs.opacity,
+        //         speed: this.starsConfigs.speed,
+        //         direction: this.starsConfigs.direction,
+        //     })
+        //     , this.level
+        // )
     }
 }
+
 
 class Star extends MovingGO {
     constructor(options = {}) {
@@ -37,7 +53,7 @@ class Star extends MovingGO {
         }, options);
 
         super(options);
-
+        
         this.fillStyle = 'rgba(255,255,255,'+this.opacity+')';
 
         this.setDestination(new V2(0, this.position.y));
@@ -54,5 +70,56 @@ class Star extends MovingGO {
     destinationCompleteCallBack() {
         this.position = new V2(SCG.scenes.activeScene.viewport.x+1, getRandomInt(0, SCG.scenes.activeScene.viewport.y));
         this.setDestination(new V2(0, this.position.y));
+    }
+}
+
+class ShiningStar extends Star {
+    constructor(options = {}){
+        options = assignDeep({}, {
+            shine: {
+                duration: 400,
+                opacityShift: 0.1,
+                opacityShiftDirection: -1,
+                start: false,
+                processer: function(){
+                    this.shine.start = !this.shine.start;
+                    if(!this.shine.start)
+                        this.opacity = this.shine.defaultOpacity;
+                }
+            }
+        }, options);
+
+        super(options);
+
+        //this.shine.timer = createTimer(this.shine.duration, this.shine.processer, this, false);
+        this.shine.defaultOpacity = this.opacity;
+    }
+
+    internalUpdate(now){
+        if(!this.shine.start){
+            this.shine.start = getRandomInt(0,1000) < 5;
+
+            if(this.shine.start)
+            {
+                this.shine.timer = createTimer(this.shine.duration, this.shine.processer, this, false);
+            }
+        }
+        else {
+            doWorkByTimer(this.shine.timer, now);
+            this.opacity += this.shine.opacityShift*this.shine.opacityShiftDirection;
+
+            if(this.opacity < 0)
+            {
+                this.opacity = 0;
+                this.shine.opacityShiftDirection = 1;
+            }
+            if(this.opacity > 1)
+            {
+                this.opacity = 1;
+                this.shine.opacityShiftDirection = -1;
+            }
+
+            this.fillStyle = 'rgba(255,255,255,'+this.opacity+')';
+        }
     }
 }
--- demo2/scenes/nightsky.js
+++ demo2/scenes/nightsky.js
@@ -5,30 +5,43 @@ class NightSkyScene extends Scene {
 
         super(options);
 
+        this.meteorRainQueue = [];
+
         new StarsLayer({starsConfigs: {
             count: 500,
-            opacity: 0.1,
+            opacity: 0.2,
             speed: 0.01
-        }, level: 0, scene: this})
+        }, level: 0, scene: this, addShiningStars: true});
 
         new StarsLayer({starsConfigs: {
             count: 300,
             opacity: 0.3,
-            speed: 0.01
-        }, level: 1, scene: this})
+            speed: 0.03
+        }, level: 1, scene: this, addShiningStars: true});
 
-        // this.addGo(new MovingStar({
-        //     path: [new V2(50,50), new V2(150,50), new V2(150,150), new V2(50,150),]
-        // }), 2);
+        new StarsLayer({starsConfigs: {
+            count: 100,
+            opacity: 0.5,
+            speed: 0.06
+        }, level: 3, scene: this, addShiningStars: true});
 
         this.timer = {
             lastTimeWork: new Date,
             delta : 0,
-            currentDelay: 2000,
-            originDelay: 2000,
+            currentDelay: 1500,
+            originDelay: 1500,
             doWorkInternal : this.objectsGenerator,
             context: this
         }
+
+        this.meteorRainTimer = createTimer(50, 
+        () => {
+            if(!this.meteorRainQueue.length)
+                return;
+
+            this.addGo(this.meteorRainQueue.shift(), 2);
+            
+        }, this);
     }
 
     objectsGenerator(){
@@ -41,31 +54,60 @@ class NightSkyScene extends Scene {
         switch(getRandomInt(0,3))
         {
             case 0:
-                position = new V2(getRandom(1, SCG.viewport.logical.width - 1), -1);
-                destination = new V2(getRandom(1, SCG.viewport.logical.width - 1), SCG.viewport.logical.height + 1);
+                position = new V2(getRandom(1, SCG.viewport.logical.width - 1), -100);
+                destination = new V2(getRandom(1, SCG.viewport.logical.width - 1), SCG.viewport.logical.height + 100);
                 break;
             case 1:
-                position = new V2(SCG.viewport.logical.width+1, getRandom(1, SCG.viewport.logical.height -1));
-                destination = new V2(-1, getRandom(1, SCG.viewport.logical.height -1));
+                position = new V2(SCG.viewport.logical.width+100, getRandom(1, SCG.viewport.logical.height -1));
+                destination = new V2(-100, getRandom(1, SCG.viewport.logical.height -1));
                 break;
             case 2:
-                position = new V2(getRandom(1, SCG.viewport.logical.width - 1), SCG.viewport.logical.height + 1);
-                destination = new V2(getRandom(1, SCG.viewport.logical.width - 1), -1);
+                position = new V2(getRandom(1, SCG.viewport.logical.width - 1), SCG.viewport.logical.height + 100);
+                destination = new V2(getRandom(1, SCG.viewport.logical.width - 1), -100);
                 break;
             case 3:
-                position = new V2(-1, getRandom(1, SCG.viewport.logical.height -1));
-                destination = new V2(SCG.viewport.logical.width+1, getRandom(1, SCG.viewport.logical.height -1));
+                position = new V2(-100, getRandom(1, SCG.viewport.logical.height -1));
+                destination = new V2(SCG.viewport.logical.width+100, getRandom(1, SCG.viewport.logical.height -1));
                 break;
         }
 
-        let size = getRandom(0.5, 1.5);
-        this.addGo(new MovingStar({
-            speed: getRandom(0.75, 2),
-            size: new V2(size, size),
-            position: position,
-            destination: destination,
-            colorSwitching: getRandomBool()
-        }), 2);
+        switch(getRandomInt(0,2)){
+            case 0:
+                let size = getRandom(0.5, 1.5);
+                this.addGo(new MovingStar({
+                    speed: getRandom(0.75, 2),
+                    size: new V2(size, size),
+                    position: position,
+                    destination: destination,
+                    colorSwitching: getRandomBool()
+                }), 2);
+                break;
+            case 1:
+                this.addGo(new Meteor({
+                    start: position,
+                    end: destination,
+                    path: [destination],
+                    speed: getRandom(10,20),
+                    length: getRandomInt(50,200),
+                    width: getRandom(0.5,1)
+                }), 2);
+                break;
+            case 2: 
+                let rainCount = getRandomInt(5,10);
+                for(let i = 0;i < rainCount;i++){
+                    let rDest = destination.add(new V2(getRandomInt(-100, 100),getRandomInt(-100, 100)));
+                    this.meteorRainQueue.push(new Meteor({
+                        start: position.add(new V2(getRandomInt(-30, 30),getRandomInt(-30, 30))),
+                        end: rDest,
+                        path: [rDest],
+                        speed: getRandom(18,22),
+                        length: getRandomInt(100,200),
+                        width: getRandom(0.3,0.6)
+                    }));
+                }
+                break;
+        }
+        
     }
 
     backgroundRender(){
@@ -75,7 +117,89 @@ class NightSkyScene extends Scene {
 
     preMainWork(now){
         doWorkByTimer(this.timer ,now);
+        doWorkByTimer(this.meteorRainTimer ,now);
     }
+}
+
+class Meteor extends MovingGO {
+    constructor (options = {}) {
+        options = assignDeep({}, {
+            speed: 1,
+            width: 0.5,
+            length: 100,
+            colors: ['rgba(175, 206, 255, 1)', 'rgba(255, 209, 238, 0.5)', 'rgba(255, 255, 255, 0)'],
+            position: new V2(),
+            size: new V2(10, 10),
+            start: new V2,
+            end: new V2(100,100),
+            isCustomRender: true,
+            setDeadOnDestinationComplete: true
+        }, options);
+
+        super(options);
+
+        this.direction = this.start.direction(this.end);
+        let dirWithLength = this.direction.mul(this.length);
+        this.points = [this.start.clone(), this.start.add(dirWithLength)];
+        this.size = new V2(Math.abs(this.points[0].x - this.points[1].x), Math.abs(this.points[0].y - this.points[1].y));
+        
+        let topLeft = new V2(Math.min(this.points[0].x,this.points[1].x), Math.min(this.points[0].y,this.points[1].y));
+        this.position = topLeft.add(this.size.division(2));
+        this.grad = undefined;
+        this.renderPoints = [new V2(), new V2()];
+    }
+
+    internalUpdate(now) {
+        if(this.renderPosition === undefined)
+            return;
+
+        if(this.direction.x > 0){
+            if(this.direction.y > 0){
+                this.grad= this.context.createLinearGradient(this.renderBox.topLeft.x, this.renderBox.topLeft.y, this.renderBox.bottomRight.x, this.renderBox.bottomRight.y);
+                this.renderPoints[0] = this.renderBox.topLeft;
+                this.renderPoints[1] = this.renderBox.bottomRight;
+            }
+            else {
+                this.grad= this.context.createLinearGradient(this.renderBox.bottomLeft.x, this.renderBox.bottomLeft.y, this.renderBox.topRight.x, this.renderBox.topRight.y);
+                this.renderPoints[0] = this.renderBox.bottomLeft;
+                this.renderPoints[1] = this.renderBox.topRight;
+            }
+        }
+        else {
+            if(this.direction.y > 0){
+                this.grad = this.context.createLinearGradient(this.renderBox.topRight.x, this.renderBox.topRight.y, this.renderBox.bottomLeft.x, this.renderBox.bottomLeft.y);
+                this.renderPoints[0] = this.renderBox.topRight;
+                this.renderPoints[1] = this.renderBox.bottomLeft;
+            }
+            else {
+                this.grad = this.context.createLinearGradient(this.renderBox.bottomRight.x, this.renderBox.bottomRight.y, this.renderBox.topLeft.x, this.renderBox.topLeft.y);
+                this.renderPoints[0] = this.renderBox.bottomRight;
+                this.renderPoints[1] = this.renderBox.topLeft;
+            }
+        }
+    }
+
+    customRender(){
+        let prevLineWidth = this.context.lineWidth;
+        this.context.lineWidth = this.width*SCG.viewport.scale;
+        let prevStrokeStyle = this.context.strokeStyle;
+
+        this.grad.addColorStop(0, this.colors[2]);
+        this.grad.addColorStop(0.5, this.colors[1]);
+        this.grad.addColorStop(1,  this.colors[0]);
+
+        this.context.strokeStyle = this.grad;
+
+        this.context.beginPath();
+        this.context.moveTo(this.renderPoints[0].x,this.renderPoints[0].y);
+        this.context.lineTo(this.renderPoints[1].x,this.renderPoints[1].y);
+        
+        this.context.stroke();
+
+        this.context.lineWidth = prevLineWidth;
+        this.context.strokeStyle = prevStrokeStyle;
+    }
+
 }
 
 class MovingStar extends MovingGO {
@@ -103,10 +227,6 @@ class MovingStar extends MovingGO {
             this.colorSwitchingTimer = createTimer(colorSwitchingTimerDelay, this.colorSwitchingProcesser, this);
         }
     }
-    
-    beforePositionChange() {
-        let foo = 'bar';
-    }
 
     colorSwitchingProcesser(){
         this.colorSwitchingDelta = [getRandom(0.1, 2)*(getRandomBool() ? 1 : -1), getRandom(0.1, 2)*(getRandomBool() ? 1 : -1), getRandom(0.1, 2)*(getRandomBool() ? 1 : -1)]
@@ -124,11 +244,19 @@ class MovingStar extends MovingGO {
         if(this.colorSwitching){
             for(let i = 0; i< 3; i++){
                 this.color[i]+=this.colorSwitchingDelta[i];
-                if(this.color[i] < 0)
-                    this.color[i] = 0
+                if(this.color[i] < 0){
+                    this.color[i] = 0;
+                    this.colorSwitchingDelta[i]*=-1;
+                }
+                    
+                if(this.color[i] > 255)
+                {
+                    this.color[i] = 255;
+                    this.colorSwitchingDelta[i]*=-1;
+                }
             }
 
-            this.fillStyle = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
+            this.fillStyle = `rgb(${parseInt(this.color[0])}, ${parseInt(this.color[1])}, ${parseInt(this.color[2])})`;
             
             doWorkByTimer(this.colorSwitchingTimer, now);
         }
