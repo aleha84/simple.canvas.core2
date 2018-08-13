6eb400582902ca7aaf5d5895c43d63869088a598
aleha84 <astarikh@mail.ru>
2018-08-13 23:29:34 +0300

    some demo features

--- demo4/scenes/children.js
+++ demo4/scenes/children.js
@@ -7,10 +7,16 @@ class ChildrenGOScene extends Scene {
 
         let star = new Star({
             position: new V2(100,100),
-            size: new V2(10,10),
-            destination: new V2(200, 100),
-            speed: 0.5
+            size: new V2(5,5),
+            //destination: new V2(200, 100),
+            //speed: 0.5
+            satellite: true,
+            colors: [255,255,255],
+            radius: 50,
+            speed: 1,
+            angle: 0
         });
+        
         star.addChild(new Star({
             position: new V2(0, -10),
             angle: 0,
@@ -20,12 +26,70 @@ class ChildrenGOScene extends Scene {
             satellite: true
         }));
 
-        this.addGo(star, 0);
+        star.addChild(new Star({
+            position: new V2(0, -10),
+            angle: 45,
+            radius: 20,
+            colors: [0,255,0],
+            speed: 1.5,
+            satellite: true
+        }));
+
+        star.addChild(new Star({
+            position: new V2(0, -10),
+            angle: 90,
+            radius: 30,
+            colors: [0,0,255],
+            speed: 2,
+            satellite: true
+        }));
+
+        let centralStar = new Star({
+            position: new V2(this.viewport.x/2,this.viewport.y/2),
+            size: new V2(10,10),
+        });
+
+        centralStar.addChild(star)
+
+        this.addGo(centralStar, 1);
     }
 
     backgroundRender(){
         SCG.contexts.background.fillStyle = 'black';
         SCG.contexts.background.fillRect(0,0,SCG.viewport.real.width,SCG.viewport.real.height);
+    }
+}
+
+class Fadeavay extends GO {
+    constructor(options = {}) {
+        options = assignDeep({}, {
+            opacity: 0.2,
+            colors: [255,255,255],
+            opacityDeltaSpeed: 0.008,
+            isCustomRender: true
+        }, options);
+
+        super(options);
+
+        this.timerProcesser = function(){
+            this.opacity-=this.opacityDeltaSpeed;
+            this.fillStyle = `rgba(${this.colors[0]},${this.colors[1]},${this.colors[2]},${this.opacity})`;
+            if(this.opacity <= 0)
+            {
+                this.opacity = 0;
+                this.setDead();
+            }
+        }
+
+        this.timer = createTimer(33, this.timerProcesser, this, false);
+    }
+
+    internalPreUpdate(now){
+        doWorkByTimer(this.timer, now);
+    }
+
+    customRender(){
+        sceneCustomRender(this)
     }
 }
 
@@ -50,11 +114,7 @@ class Star extends MovingGO {
     }
 
     customRender(){
-        this.context.fillStyle = this.fillStyle;
-        let rp = this.renderPosition;
-        let rsx = this.renderSize.x;
-        let rsy = this.renderSize.y;
-        this.context.fillRect(rp.x - rsx/2, rp.y - rsy/2, rsx,rsy);
+        sceneCustomRender(this)
     }
 
     beforePositionChange(now){
@@ -67,4 +127,38 @@ class Star extends MovingGO {
             this.needRecalcRenderProperties = true;
         }
     }
+
+    internalUpdate(now){
+        SCG.scenes.activeScene.addGo(new Fadeavay({
+            position: (this.parent ? this.absolutePosition : this.position).clone(),
+            size: this.size.clone(),
+            colors: this.colors
+        }), 1);
+    }
+}
+
+function sceneCustomRender(go) {
+    let _fillStyle = go.context.fillStyle;
+    let _strokeStyle = go.context.strokeStyle;
+
+    go.context.fillStyle = go.fillStyle;
+    if(go.renderSize.x > 3){
+        let center = go.renderPosition;
+        let diameter = go.renderSize.x;
+        go.context.beginPath();
+        go.context.arc(center.x, center.y, diameter/2, 0, 2 * Math.PI, false);
+        go.context.fill();
+        go.context.strokeStyle = go.fillStyle;
+        go.context.stroke();
+    }
+    else 
+    {
+        let rp = go.renderPosition;
+        let rsx = go.renderSize.x;
+        let rsy = go.renderSize.y;
+        go.context.fillRect(rp.x - rsx/2, rp.y - rsy/2, rsx,rsy);
+    }
+
+    go.context.fillStyle = _fillStyle;
+    go.context.strokeStyle = _strokeStyle;
 }
