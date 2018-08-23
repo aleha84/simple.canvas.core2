71e7eefdb9383c9403a670003f3cd96d8ee04fbf
Starykh Aleksey <astarikh@mail.ru>
2018-08-23 18:14:11 +0300

    demo generators for tilemap

---
+++ demo3/scenes/terrainAtlasMappings.js
@@ -0,0 +1,33 @@
+function terrainAtlasMappings(defaultSourceTileSize) {
+    let initialCentralPositions = {
+        'highGrassCentral': {
+            destSourcePosition: new V2(32,736),
+            destSourceSize: defaultSourceTileSize.clone()
+        },
+        'greenBackCentral': {
+            destSourcePosition: new V2(704,96),
+            destSourceSize: defaultSourceTileSize.clone()
+        }
+    }
+
+    let result = {};
+
+    Object.keys(initialCentralPositions).forEach(propName => {
+        let centralMap = initialCentralPositions[propName];
+        result[propName] = centralMap;
+
+        let ds = defaultSourceTileSize;
+        let centralMapSP = centralMap.destSourcePosition;
+        let propNameWithoutCentral = propName.replace('Central', '');
+        result[propNameWithoutCentral + 'TopLeft'] = { destSourcePosition: centralMapSP.add(new V2(-ds.x, -ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'Top'] = { destSourcePosition: centralMapSP.add(new V2(0, -ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'TopRight'] = { destSourcePosition: centralMapSP.add(new V2(ds.x, -ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'Right'] = { destSourcePosition: centralMapSP.add(new V2(ds.x, 0)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'BottomRight'] = { destSourcePosition: centralMapSP.add(new V2(ds.x, ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'Bottom'] = { destSourcePosition: centralMapSP.add(new V2(0, ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'BottomLeft'] = { destSourcePosition: centralMapSP.add(new V2(-ds.x, ds.y)), destSourceSize: ds.clone() }
+        result[propNameWithoutCentral + 'Left'] = { destSourcePosition: centralMapSP.add(new V2(-ds.y, 0)), destSourceSize: ds.clone() }
+    });
+
+    return result;
+}
--- demo3/scenes/tilemap.js
+++ demo3/scenes/tilemap.js
@@ -8,26 +8,24 @@ class TileMapScene extends Scene {
         this.ts = new TileSet({
             imgPropertyName: 'terrain_atlas',
             tileSize: new V2(25,25),
-            tilesMappings: {
-                'highGrassCentral': {
-                    destSourcePosition: new V2(32,736),
-                    destSourceSize: defaultSourceTileSize.clone()
-                },
-                'cs': {
-                    destSourcePosition: new V2(),
-                    destSourceSize: new V2(20,20)
-                },
-                'gs': {
-                    destSourcePosition: new V2(20,0),
-                    destSourceSize: new V2(20,20)
-                },
-                'lamp': {
-                    destSourcePosition: new V2(0,21),
-                    destSourceSize: new V2(21,68),
-                    noTileAdjustment: true,
-                    size: new V2(21,68)
-                }
-            },
+            tilesMappings: terrainAtlasMappings(defaultSourceTileSize),
+            // {
+                
+            //     'cs': {
+            //         destSourcePosition: new V2(),
+            //         destSourceSize: new V2(20,20)
+            //     },
+            //     'gs': {
+            //         destSourcePosition: new V2(20,0),
+            //         destSourceSize: new V2(20,20)
+            //     },
+            //     'lamp': {
+            //         destSourcePosition: new V2(0,21),
+            //         destSourceSize: new V2(21,68),
+            //         noTileAdjustment: true,
+            //         size: new V2(21,68)
+            //     }
+            // },
             tilesMatrix: this.matrixGenerator(),
             // [
             //     ['cs', 'cs', 'cs', 'cs', 'cs', 'cs'],
@@ -42,12 +40,32 @@ class TileMapScene extends Scene {
     }
 
     matrixGenerator(){
-        //initial layer
+        //initial layer 0
         var result = new Array(this.size.y);
         for(let rowIndex = 0; rowIndex < this.size.y; rowIndex++){
             result[rowIndex] = new Array(this.size.x);
             for(let columnIndex = 0; columnIndex < this.size.x; columnIndex++){
-                result[rowIndex][columnIndex] = { type: 'highGrassCentral' }
+                result[rowIndex][columnIndex] = { type: 'greenBackCentral' }
+            }
+        }
+
+        //highgrasslayer 0
+        let vertices = [new V2(3,3), new V2(7,4), new V2(4,7)]
+        //draw lines
+        for(let vi = 0; vi < vertices.length; vi++){
+            let from = vertices[vi];
+            let to = (vi == vertices.length - 1 ? vertices[0] : vertices[vi+1]);
+
+            let deltas = new V2(to.x - from.x, to.y - from.y);
+            let maxDelta = Math.max(Math.abs(deltas.x), Math.abs(deltas.y));
+            let step = new V2(deltas.x/maxDelta, deltas.y/maxDelta);
+            let current = from.clone();
+            //result[from.y][from.x] = { type: 'highGrassCentral' };
+            for(let i = 0; i < maxDelta;i++){
+                current = current.add(step);
+                // current.x = Math.round(current.x);
+                // current.y = Math.round(current.y);
+                result[Math.round(current.y)][Math.round(current.x)] = { type: 'highGrassCentral' };
             }
         }
 
