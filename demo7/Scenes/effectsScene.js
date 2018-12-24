2f4f63f0e5100f1ac5f882b894bc0ef8a472e2d3
aleha84 <astarikh@mail.ru>
2018-12-24 23:45:26 +0300

    minor refactoring

--- demo7/Scenes/effectsScene.js
+++ demo7/Scenes/effectsScene.js
@@ -15,12 +15,21 @@ class EffectsScene extends Scene {
 
         for(let r = 0; r < this.vCount; r++){
             for(let c = 0; c < this.hCount; c++){
-                this.addGo(new DemoObject({
+                let go = new DemoObject({
                     size: this.itemSize,
                     position: new V2(this.itemSize.x*c + this.itemSize.x/2, this.itemSize.y*r + this.itemSize.y/2),
                     img: this.itemImg,
-                    addDelay: c%2 == 0
-                }))
+                    //addDelay: c%2 == 0
+                });
+
+                if(c%2==0){
+                    go.addEffect(new FadeOutEffect({updateDelay: 50, effectTime: 5000, completeCallback: () => console.log('fadeout completed')}))
+                }
+                else {
+                    go.addEffect(new FadeInEffect({updateDelay: 50, effectTime: 5000, completeCallback: () => console.log('fadein completed')}))
+                }
+
+                this.addGo(go)
             }
         }
     }
@@ -39,173 +48,11 @@ class DemoObject extends GO {
 
         super(options);
 
-        this.addEffect(new FadeOutEffect({
-            startDelay: this.addDelay ? 2000 : 0,
-            updateDelay: 50,
-            effectTime: 5000
-        }));
+        // this.addEffect(new FadeOutEffect({
+        //     startDelay: this.addDelay ? 2000 : 0,
+        //     updateDelay: 50,
+        //     effectTime: 5000
+        // }));
     }
 }
 
-class EffectBase {
-    constructor(options = {}){
-        assignDeep(this, {
-            enabled: true,
-            startDelay: 0,
-            completed: false,
-            removeEffectOnComplete: false,
-            setParentDeadOnComplete: false,
-            disableEffectOnComplete: false,
-            startImmediately: true,
-            mustRemove: false,
-            workTimer: undefined,
-            delayTimer: undefined
-        }, options)
-
-        if(this.startDelay > 0){
-            this.enabled = false;
-            this.delayTimer = createTimer(this.startDelay, 
-                () => { this.delayTimer = undefined; this.enabled = true; }, 
-                this, false);
-        }
-
-        this.__completeCallback = () => { 
-            this.completed = true;
-
-            if(this.setParentDeadOnComplete){
-                this.parent.setDead();
-            }
-
-            if(this.removeEffectOnComplete){ //TODO: test
-                this.mustRemove = true;
-                this.enabled = false;
-            }
-
-            if(this.disableEffectOnComplete){
-                this.enabled = false;
-            }
-
-            this.completeCallback();
-        }
-    }
-
-    init(parent){
-        this.parent = parent;
-        this.context = this.parent.context;
-    }
-
-    beforeRender(){}
-        
-    afterRender(){}
-
-    beforeUpdate(){}
-        
-    afterUpdate(now){
-        if(this.delayTimer){
-            doWorkByTimer(this.delayTimer, now);
-        }
-
-        if(!this.enabled)
-            return;
-
-        if(this.workTimer){
-            doWorkByTimer(this.workTimer, now);
-        }
-    }
-
-    completeCallback() {
-
-    }
-}
-
-class FadeInEffect extends EffectBase {
-    constructor(options = {}){
-        options = assignDeep({}, {
-            effectTime: undefined,
-            step: 0.05,
-            current: 0,
-            updateDelay: 100,
-            originalGlobalAlpha: undefined
-        }, options)
-
-        super(options);
-
-        if(this.effectTime) {
-            this.step = 1/ (this.effectTime/this.updateDelay);
-        }
-
-        this.workTimer = createTimer(this.updateDelay, () => {
-            if(this.completed)
-                return;
-
-            this.current+=this.step;
-
-            if(this.current >= 1){
-                this.current = 1;
-
-                this.__completeCallback();
-            }
-        }, this, this.startImmediately);
-    }
-
-    beforeRender() {
-        if(!this.enabled)
-            return;
-
-        this.originalGlobalAlpha = this.context.globalAlpha;
-        this.context.globalAlpha = this.current;
-    }
-
-    afterRender(){
-        if(!this.enabled)
-            return;
-
-        this.context.globalAlpha = this.originalGlobalAlpha;
-    }
-}
-
-class FadeOutEffect extends EffectBase {
-    constructor(options = {}){
-        options = assignDeep({}, {
-            effectTime: undefined,
-            step: 0.05,
-            current: 1,
-            updateDelay: 100,
-            originalGlobalAlpha: undefined
-        }, options)
-
-        super(options);
-
-        if(this.effectTime) {
-            this.step = 1/ (this.effectTime/this.updateDelay);
-        }
-
-        this.workTimer = createTimer(this.updateDelay, () => {
-            if(this.completed)
-                return;
-
-            this.current-=this.step;
-
-            if(this.current <= 0){
-                this.current = 0;
-
-                this.__completeCallback();
-            }
-        }, this, this.startImmediately);
-    }
-
-    beforeRender() {
-        if(!this.enabled)
-            return;
-
-        this.originalGlobalAlpha = this.context.globalAlpha;
-        this.context.globalAlpha = this.current;
-    }
-
-    afterRender(){
-        if(!this.enabled)
-            return;
-
-        this.context.globalAlpha = this.originalGlobalAlpha;
-    }
-}
---
+++ models/effects.js
@@ -0,0 +1,162 @@
+class EffectBase {
+    constructor(options = {}){
+        assignDeep(this, {
+            enabled: true,
+            startDelay: 0,
+            completed: false,
+            removeEffectOnComplete: false,
+            setParentDeadOnComplete: false,
+            disableEffectOnComplete: false,
+            startImmediately: true,
+            mustRemove: false,
+            workTimer: undefined,
+            delayTimer: undefined
+        }, options)
+
+        if(this.startDelay > 0){
+            this.enabled = false;
+            this.delayTimer = createTimer(this.startDelay, 
+                () => { this.delayTimer = undefined; this.enabled = true; }, 
+                this, false);
+        }
+
+        this.__completeCallback = () => { 
+            this.completed = true;
+
+            if(this.setParentDeadOnComplete){
+                this.parent.setDead();
+            }
+
+            if(this.removeEffectOnComplete){ //TODO: test
+                this.mustRemove = true;
+                this.enabled = false;
+            }
+
+            if(this.disableEffectOnComplete){
+                this.enabled = false;
+            }
+
+            this.completeCallback();
+        }
+    }
+
+    init(parent){
+        this.parent = parent;
+        this.context = this.parent.context;
+    }
+
+    beforeRender(){}
+        
+    afterRender(){}
+
+    beforeUpdate(){}
+        
+    afterUpdate(now){
+        if(this.delayTimer){
+            doWorkByTimer(this.delayTimer, now);
+        }
+
+        if(!this.enabled)
+            return;
+
+        if(this.workTimer){
+            doWorkByTimer(this.workTimer, now);
+        }
+    }
+
+    completeCallback() {
+
+    }
+}
+
+class FadeInEffect extends EffectBase {
+    constructor(options = {}){
+        options = assignDeep({}, {
+            effectTime: undefined,
+            step: 0.05,
+            current: 0,
+            updateDelay: 100,
+            originalGlobalAlpha: undefined
+        }, options)
+
+        super(options);
+
+        if(this.effectTime) {
+            this.step = 1/ (this.effectTime/this.updateDelay);
+        }
+
+        this.workTimer = createTimer(this.updateDelay, () => {
+            if(this.completed)
+                return;
+
+            this.current+=this.step;
+
+            if(this.current >= 1){
+                this.current = 1;
+
+                this.__completeCallback();
+            }
+        }, this, this.startImmediately);
+    }
+
+    beforeRender() {
+        if(!this.enabled)
+            return;
+
+        this.originalGlobalAlpha = this.context.globalAlpha;
+        this.context.globalAlpha = this.current;
+    }
+
+    afterRender(){
+        if(!this.enabled)
+            return;
+
+        this.context.globalAlpha = this.originalGlobalAlpha;
+    }
+}
+
+class FadeOutEffect extends EffectBase {
+    constructor(options = {}){
+        options = assignDeep({}, {
+            effectTime: undefined,
+            step: 0.05,
+            current: 1,
+            updateDelay: 100,
+            originalGlobalAlpha: undefined
+        }, options)
+
+        super(options);
+
+        if(this.effectTime) {
+            this.step = 1/ (this.effectTime/this.updateDelay);
+        }
+
+        this.workTimer = createTimer(this.updateDelay, () => {
+            if(this.completed)
+                return;
+
+            this.current-=this.step;
+
+            if(this.current <= 0){
+                this.current = 0;
+
+                this.__completeCallback();
+            }
+        }, this, this.startImmediately);
+    }
+
+    beforeRender() {
+        if(!this.enabled)
+            return;
+
+        this.originalGlobalAlpha = this.context.globalAlpha;
+        this.context.globalAlpha = this.current;
+    }
+
+    afterRender(){
+        if(!this.enabled)
+            return;
+
+        this.context.globalAlpha = this.originalGlobalAlpha;
+    }
+}
