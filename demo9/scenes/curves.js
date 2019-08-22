af1b23d09c221c0f2188e5868c83d1a89a388091
Starykh Aleksey <astarikh@mail.ru>
2019-08-22 11:00:41 +0300

    small fountain scene completed

--- demo9/scenes/curves.js
+++ demo9/scenes/curves.js
@@ -2,7 +2,7 @@ class CurvesScene extends Scene {
     constructor(options = {}) {
         options = assignDeep({}, {
             debug: {
-                enabled: true,
+                enabled: false,
                 showFrameTimeLeft: true,
                 additional: [],
             },
@@ -14,7 +14,7 @@ class CurvesScene extends Scene {
         this.backgroundRenderDefault();
     }
 
-    createBrick(hlp, currentX, currentY, brickSize) {
+    createBrick(hlp, currentX, currentY, brickSize, dotsCount = 20, removeCorners = false) {
         hlp.setFillColor(colors.palettes.fleja.colors[8]).rect(currentX, currentY, brickSize.x, brickSize.y);
         hlp.setFillColor(colors.palettes.fleja.colors[7]).rect(currentX, currentY, 1, brickSize.y)
                                                         .rect(currentX, currentY, brickSize.x, 1)
@@ -36,8 +36,12 @@ class CurvesScene extends Scene {
             }
         }
 
-        for(let i = 0; i < 20; i++){
+        for(let i = 0; i < dotsCount; i++){
             hlp.setFillColor(colors.palettes.fleja.colors[getRandomBool() ? 7 : 9]).dot(currentX + getRandomInt(2, brickSize.x - 4), currentY + getRandomInt(2, brickSize.y - 4), )
+        }
+
+        if(removeCorners) {
+            hlp.clear(currentX, currentY).clear(currentX+brickSize.x-1, currentY).clear(currentX, currentY+brickSize.y-1).clear(currentX+brickSize.x-1, currentY+brickSize.y-1);
         }
     }
 
@@ -58,6 +62,12 @@ class CurvesScene extends Scene {
                 // })
 
                 this.wideLine.forEach(l => l.position = l.originPosition);
+
+                this.splashes = new Array(40).fill().map(() => ({
+                    position: new V2(getRandomGaussian(0, this.size.x), this.size.y-1),
+                    speed: new V2((getRandomBool() ? -1 : 1)*getRandomInt(1,4), getRandomInt(-1,-4)),
+                    color: colors.palettes.fleja.colors[getRandomInt(4,6)]
+                }))
 
                 this.timer = this.regTimerDefault(15, () => {
                     for(let i = 0; i < this.lines.length;i++){
@@ -86,6 +96,19 @@ class CurvesScene extends Scene {
                             this.wideLine[i].changeX = undefined;
                         }
                     }
+
+                    for(let i = 0; i < this.splashes.length;i++){
+                        let splash = this.splashes[i];
+                        splash.position.add(splash.speed, true);
+                        splash.speed.y+=0.3;
+
+                        if(splash.position.y > this.size.y) {
+                            splash.position = new V2(getRandomGaussian(0, this.size.x), this.size.y-1);
+                            splash.speed = new V2((getRandomBool() ? -1 : 1)*getRandomInt(1,4), getRandomInt(-1,-4));
+                            splash.color = colors.palettes.fleja.colors[getRandomInt(4,6)];
+                        }
+                    }
+                
 
                     this.createImg();
                 })
@@ -131,9 +154,27 @@ class CurvesScene extends Scene {
                                     else if(h == hCount-3){
                                         hlp.setFillColor('rgba(0,0,0,0.25').rect(currentX-1-fast.r(brickSize.x/10), currentY-1+fast.r(brickSize.y/10), brickSize.x+2,brickSize.y+2)
                                     }
-                                    hlp.setFillColor(colors.palettes.fleja.colors[10]).rect(currentX-1,currentY-1,brickSize.x+2,brickSize.y+2);
-                                    this.parentScene.createBrick(hlp, currentX, currentY, brickSize);
+                                    if(h==1){
+                                        if(v%2 !== 0){
+
+                                        }
+                                        else {
+                                            hlp.setFillColor(colors.palettes.fleja.colors[10]).rect(currentX-1,currentY-2,brickSize.x+2,brickSize.y+4);
+                                        }
+                                    }
+                                    else if(h == hCount-3){
+                                        if(v%2 !== 0){
+                                            hlp.setFillColor(colors.palettes.fleja.colors[10]).rect(currentX-1,currentY-2,brickSize.x+2,brickSize.y+4);
+                                        }
+                                        else {
+                                            
+                                        }
+                                    }
+                                    else{
+                                        hlp.setFillColor(colors.palettes.fleja.colors[10]).rect(currentX-2,currentY-2,brickSize.x+4,brickSize.y+4);
+                                    }
                                     
+                                    this.parentScene.createBrick(hlp, currentX, currentY, brickSize);
                                 }
 
                                 currentX+=(brickSize.x+2);
@@ -167,6 +208,12 @@ class CurvesScene extends Scene {
                         // }
                     }
 
+                    for(let i = 0; i < this.splashes.length;i++){
+                        let splash = this.splashes[i];
+                        hlp.setFillColor(splash.color).rect(fast.r(splash.position.x), fast.r(splash.position.y), 4,4)
+                        
+                    }
+
                     ctx.drawImage(this.fg, 0,0,size.x, size.y);
                 })
             }
@@ -175,6 +222,7 @@ class CurvesScene extends Scene {
         this.frontalBricks = this.addGo(new GO({
             position: new V2(this.sceneCenter.x, this.sceneCenter.y+20),
             size: new V2(this.viewport.x, 50),
+            //isVisible: false,
             init() {
                 this.img = createCanvas(this.size, (ctx, size, hlp) => {
                     //hlp.setFillColor('red').rect(0,0,size.x, size.y)
@@ -187,7 +235,7 @@ class CurvesScene extends Scene {
                     for(let v = 0; v < vCount;v++){
                         let currentX = v%2 == 0 ? getRandomInt(-brickSize.x, -brickSize.x/2) : getRandomInt(-brickSize.x/2, 0)
                         for(let h = 0; h < hCount; h++){
-                            this.parentScene.createBrick(hlp, currentX, currentY, brickSize);
+                            this.parentScene.createBrick(hlp, currentX, currentY, brickSize, 50, true);
 
                             currentX+=(brickSize.x);
                         }
@@ -196,7 +244,7 @@ class CurvesScene extends Scene {
                     }
                  })
             }
-        }), 5)
+        }), 20)
 
         this.lowerBricks = this.addGo(new GO({
             position: new V2(this.sceneCenter.x, 240),
@@ -213,7 +261,7 @@ class CurvesScene extends Scene {
                     for(let v = 0; v < vCount;v++){
                         let currentX = v%2 == 0 ? getRandomInt(-brickSize.x, -brickSize.x/2) : getRandomInt(-brickSize.x/2, 0)
                         for(let h = 0; h < hCount; h++){
-                            this.parentScene.createBrick(hlp, currentX, currentY, brickSize);
+                            this.parentScene.createBrick(hlp, currentX, currentY, brickSize, 15);
 
                             currentX+=(brickSize.x+1);
                         }
@@ -230,52 +278,120 @@ class CurvesScene extends Scene {
             }
         }), 3)
 
-        this.waterLine = this.addGo(new GO({
-            position: this.sceneCenter.clone(),
-            size: this.viewport.clone(),
-            createImg(yChange) {
-                this.img = createCanvas(this.size, (ctx, size, hlp) => {
-                    let start = new V2(100, 100);
-                    let end = new V2(150, 100);
-                    //let midPoints = [{distance: 0.8/3, yChange: 50}, {distance: 2.2/3, yChange: 100}]
-                    let midPoints = [{distance: 1/2, yChange: yChange}]
-    
-                    let dots = mathUtils.getCurvePoints({start, end, midPoints, startMethod: 'inOut', endMethod: 'inOut'});
-                    
-    
-                    hlp.setFillColor('red');
-                    let pp = new PerfectPixel({context: ctx});
-                    for(let i = 1; i < dots.length; i++){
-                        pp.lineV2(dots[i], dots[i-1]);
-                        // hlp.setFillColor('yellow').dot(rotatedDots[i].x, rotatedDots[i].y);
-                        // hlp.setFillColor('red').dot(dots[i].x, dots[i].y);
-                    }
+        this.waterDrops = [];
+        this.waterDropsGenerator = this.regTimerDefault(150, () => {
+            this.waterDrops = this.waterDrops.filter(wd => wd.alive);
+
+            if(this.waterDrops.length < 6){
+                let position = fast.r(getRandomGaussian(0, this.viewport.x));
+                let width = getRandomInt(20, 50);
+                let nwdStart = position-width/2
+                let nwdEnd = position+width/2
+
+                let intersctions = this.waterDrops.filter(wd => {
+                    let wdStart = wd.position.x-wd.size.x/2;
+                    let wdEnd = wd.position.x+wd.size.x/2;
+
+                    return (nwdStart >= wdStart && nwdStart <= wdEnd) || (nwdEnd >= wdStart && nwdEnd <= wdEnd) || 
+                    (wdStart >= nwdStart && wdStart <= nwdEnd) || (wdEnd >= nwdStart && wdEnd <= nwdEnd);
                 })
-            }
-        }),10)
 
-        this.waterLineYCurrent = 0;
-        this.waterLineYTimeMax = 100;
-        this.waterLineYGoBackTimeMax = 10;
-        this.waterLineYMax = 25;
-        this.waterLineYChange = easing.createProps(this.waterLineYTimeMax, 0, this.waterLineYMax, 'quad', 'out');
-        this.waterLineYGoBackChange = easing.createProps(this.waterLineYGoBackTimeMax, this.waterLineYMax, 0, 'quad', 'out');
-
-        this.currentChange = this.waterLineYChange;
-        this.isGoBack = false;
-
-        this.timer = this.regTimerDefault(15, () => {
-            //this.currentChange.time = this.waterLineYCurrent;
-            this.waterLine.createImg(easing.process(this.currentChange));
-
-            this.currentChange.time++;
-
-            if(this.currentChange.time > this.currentChange.duration){
-                this.waterLineYCurrent = 0;
-                this.isGoBack = !this.isGoBack;
+                if(intersctions.length == 0){
+                    this.waterDrops.push(this.addGo(new GO({
+                        position: new V2(position, 254),
+                        size: new V2(width, 120),//this.viewport.clone(),
+                        waterLineYTimeMax: getRandomInt(70,100), 
+                        waterLineYGoBackTimeMax: 10,
+                        waterLineYMax: 25,
+                        
+                        init() {
+                            this.waterLineYGoBackTimeMax = fast.r(this.waterLineYTimeMax/10);
+                            this.waterLineYMax = this.size.x/2;
+                            this.waterLineYCurrent = 0;
+                            this.waterLineYChange = easing.createProps(this.waterLineYTimeMax, 0, this.waterLineYMax, 'quad', 'out');
+                            this.waterLineYGoBackChange = easing.createProps(this.waterLineYGoBackTimeMax, this.waterLineYMax, 0, 'quad', 'out');
+                    
+                            this.currentChange = this.waterLineYChange;
+                            this.isGoBack = false;
+                    
+                            this.timer = this.regTimerDefault(15, () => {
+                                //this.currentChange.time = this.waterLineYCurrent;
+                                this.createImg(easing.process(this.currentChange));
+                    
+                                this.currentChange.time++;
+            
+                                if(this.drop){
+                                    this.drop.position.y+=10;
+            
+                                    if(this.drop.position.y > this.size.y + this.drop.size.y){
+                                        this.drop = undefined;
+                                    }
+                                }
+                    
+                                if(this.currentChange.time > this.currentChange.duration){
+                                    if(this.isGoBack){
+                                        this.setDead();
+                                        return;
+                                    }
+            
+                                    this.waterLineYCurrent = 0;
+                                    this.isGoBack = true;//!this.isGoBack;
+                                    
+                                    this.currentChange = this.isGoBack ? this.waterLineYGoBackChange : this.waterLineYChange;
+                                    this.currentChange.time = 0;
+            
+                                    if(this.isGoBack && !this.drop){
+                                        this.drop = {
+                                            position: new V2(this.size.x/2, this.waterLineYMax + this.size.x/3),
+                                            size: new V2(this.size.x/6, this.size.x/4).toInt()
+                                        }
+                                    }
+                                }
+                            })
+                        },
+                        createImg(yChange) {
+                            this.img = createCanvas(this.size, (ctx, size, hlp) => {
+                                let start = new V2(0, 0);//new V2(100, 194);
+                                let end = new V2(this.size.x, 0);//new V2(150, 194);
+                                //let midPoints = [{distance: 0.8/3, yChange: 50}, {distance: 2.2/3, yChange: 100}]
+                                let midPoints = [{distance: 1/2, yChange: yChange}]
                 
-                this.currentChange = this.isGoBack ? this.waterLineYGoBackChange : this.waterLineYChange;
-                this.currentChange.time = 0;
+                                let dots = mathUtils.getCurvePoints({start, end, midPoints, startMethod: 'inOut', endMethod: 'inOut'});
+                                
+                
+                                hlp.setFillColor(colors.palettes.fleja.colors[23]);
+                                let pp = new PerfectPixel({context: ctx});
+                                let points = []
+                                for(let i = 1; i < dots.length; i++){
+                                    points = [...points, ...pp.lineV2(dots[i], dots[i-1])];
+                                    // hlp.setFillColor('yellow').dot(rotatedDots[i].x, rotatedDots[i].y);
+                                    // hlp.setFillColor('red').dot(dots[i].x, dots[i].y);
+                                }
+            
+                                //ctx.globalAlpha = 0.75;
+                                points = distinct(points, (p) => p.x+'_'+p.y);
+                                for(let i = 0; i < points.length; i++){
+                                    hlp.setFillColor(colors.palettes.fleja.colors[22]).rect(fast.r(points[i].x), 0, 1, fast.r(points[i].y))
+
+                                    if(points[i].x > size.x/3 && points[i].x < size.x*2/3){
+                                        hlp.setFillColor(colors.palettes.fleja.colors[23]).dot(fast.r(points[i].x), fast.r(points[i].y-4))
+
+                                    }
+
+                                    // if(points[i].x > size.x*9/20 && points[i].x < size.x*11/20){
+                                    //     hlp.setFillColor(colors.palettes.fleja.colors[25]).dot(fast.r(points[i].x), fast.r(points[i].y-5))
+                                    // }
+                                }
+                                //ctx.globalAlpha = 1;
+            
+                                if(this.drop){
+                                    hlp.setFillColor(colors.palettes.fleja.colors[23]);
+                                    hlp.elipsis(this.drop.position, this.drop.size)
+                                }
+                            })
+                        }
+                    }), 10));
+                }
             }
         })
     }
