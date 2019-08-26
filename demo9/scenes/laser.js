95c14266e8d87cea418b22a6e819aefd32d7f98a
aleha84 <astarikh@mail.ru>
2019-08-26 23:59:14 +0300

    laser scene update

--- demo9/scenes/laser.js
+++ demo9/scenes/laser.js
@@ -19,22 +19,46 @@ class LaserScene extends Scene {
             position: this.sceneCenter.clone(),
             size: this.viewport.clone(),
             init() {
-                this.vector = new V2(-0.5,1);
-                this.width = fast.r(this.size.x*0.8);
-                this.xStart = (this.size.x - this.width)/2;
-                this.fromY = fast.r(this.size.y/4);
-                this.shift = new V2(300, 200);
+                this.endsCache = [];
+                this.direction = new V2(-0.8,0.5);
+                this.width = fast.r(this.size.x*0.7);
+                this.ls = new Array(this.width).fill(350);
+                this.startFrom = new V2(this.size.x-1, fast.r(this.size.y/4))
+                this.starts = new Array(this.width).fill().map((_,i) => {
+                    return this.startFrom.add(new V2(i, 0));
+                })
+
+                this.laser = {
+                    from: new V2(this.size.x/3, 0).toInt(),
+                    color: '#FF5842',
+                    to: {
+                        vx: 150,
+                        length: 300
+                    }
+                }
 
                 this.createImage();
+            },
+            getEnd(length) {
+                if(!this.endsCache[length]){
+                    this.endsCache[length] = this.direction.mul(length);
+                }
+
+                return this.endsCache[length];
             },
             createImage() {
                 this.img = createCanvas(this.size, (ctx, size, hlp) => {
                     let pp = new PerfectPixel({context: ctx});
-                    hlp.setFillColor('white');
+                    hlp.setFillColor('#F1F1F1');
 
-                    for(let x = this.xStart; x<this.width; x++ ){
-                        pp.line(x, this.fromY+this.shift.y, x+this.shift.x, this.fromY);
+                    for(let i = 0; i< this.starts.length;i++){
+                        pp.lineV2(this.starts[i], this.starts[i].add(this.getEnd(this.ls[i])));
                     }
+
+                    hlp.setFillColor(this.laser.color);
+                    let laserTo = this.starts[this.laser.to.vx].add(this.getEnd(this.laser.to.length));
+                    pp.lineV2(this.laser.from, laserTo)
+                    pp.lineV2(this.laser.from.add(new V2(-1,0)), laserTo.add(new V2(-1,0)));
                 })
             }
         }))
