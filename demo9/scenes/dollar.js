0afc15c39796002b978383103e11bc97596b9330
Starykh Aleksey <astarikh@mail.ru>
2019-09-03 19:00:44 +0300

    dollar scene updated

--- demo9/scenes/dollar.js
+++ demo9/scenes/dollar.js
@@ -2,7 +2,7 @@ class DollarScene extends Scene {
     constructor(options = {}) {
         options = assignDeep({}, {
             debug: {
-                enabled: true,
+                enabled: false,
                 showFrameTimeLeft: true,
                 additional: [],
             },
@@ -16,15 +16,131 @@ class DollarScene extends Scene {
 
     start(){
         this.dollar = this.addGo(new GO({
+            hsvCache: [],
             position: this.sceneCenter.clone(),
             size: this.viewport.clone(),
             init() {
+                this.paths = [];
+
+                this.paths.push(this.generatePath())
+
                 
-                this.start = new V2(this.size.x/2, this.size.y/3);
-                this.end = new V2(this.size.x/2, this.size.y)
-                this.midPoints = [{distance: 1/60, yChange: -10}, {distance: 2/20, yChange: 20}, {distance: 5/20, yChange: -70}, {distance: 8/20, yChange: 100}, {distance: 15/20, yChange: -100}]
+                this.pathsGeneratorTimer = this.regTimerDefault(150, () => {
+                    if(getRandomInt(0,1) == 0) {
+                        this.paths.push(this.generatePath())
+
+                        // this.pathImg= createCanvas(this.size, (ctx, size, hlp) => {
+                        //     hlp.setFillColor('gray');
+                        //     for(let j = 0; j < this.paths.length; j++){
+                        //         for(let i = 0; i < this.paths[j].points.length;i++){
+                        //             hlp.dot(this.paths[j].points[i].x, this.paths[j].points[i].y)
+                        //         }
+                        //     }
+                        // })
+                    }
+                })
+
+                this.itemsProcesserTimer = this.regTimerDefault(15, () => {
+                    for(let j = 0; j < this.paths.length; j++){
+                        let reverse = this.paths[j].reverse;
+                        if(this.paths[j].itemsCreated < this.paths[j].maxItems){
+                            let time = fast.r(this.paths[j].points.length*getRandom(0.45,0.75));
+                            this.paths[j].items.push({
+                                isWhite: getRandomInt(0,20) == 0,
+                                indexChange: reverse
+                                    ? easing.createProps(time, 0, this.paths[j].points.length-1, 'quad', 'in')
+                                    : easing.createProps(time, this.paths[j].points.length-1, 0, 'quad', 'out'),
+                                xShiftChange: reverse 
+                                    ? easing.createProps(time, 1, getRandomInt(10,50), 'quad', 'in')
+                                    : easing.createProps(time, getRandomInt(10,50), 1, 'quad', 'out'),
+                                yShiftChange: reverse 
+                                    ? easing.createProps(time, 1, getRandomInt(10,50), 'quad', 'in')
+                                    : easing.createProps(time, getRandomInt(10,50), 1, 'quad', 'out'),
+                                sizeChange: reverse
+                                    ? easing.createProps(time, 1, getRandomInt(3,5), 'quad', 'in')
+                                    : easing.createProps(time, getRandomInt(3,5), 1, 'quad', 'out'),
+                                vChange: reverse
+                                    ? easing.createProps(time, 0, 100, 'quad', 'out')
+                                    : easing.createProps(time, 100, 0, 'quad', 'in'),
+                                sChange: reverse
+                                    ? easing.createProps(time, 50, 100, 'quad', 'out')
+                                    : easing.createProps(time, 100, 50, 'quad', 'in'),
+                                alive: true
+                            })
+    
+                            this.paths[j].itemsCreated++;
+                        }
+                        else {
+                            this.paths[j].alive = this.paths[j].items != 0;
+                        }
+
+                        for(let i = 0; i < this.paths[j].items.length; i++){
+                            let item = this.paths[j].items[i];
+                            if((!reverse && item.index == 0) || (reverse && item.index == this.paths[j].points.length-1)){
+                                item.alive = false;
+                                continue;
+                            }
+                             
+                            easing.commonProcess({context: item, targetpropertyName: 'index', propsName: 'indexChange', round: true})
+                            easing.commonProcess({context: item, targetpropertyName: 'xShift', propsName: 'xShiftChange', round: true})
+                            easing.commonProcess({context: item, targetpropertyName: 'yShift', propsName: 'yShiftChange', round: true})
+                            easing.commonProcess({context: item, targetpropertyName: 'size', propsName: 'sizeChange', round: true})
+                            easing.commonProcess({context: item, targetpropertyName: 'v', propsName: 'vChange', round: true})
+                            easing.commonProcess({context: item, targetpropertyName: 's', propsName: 'sChange', round: true})
+    
+                        }
+
+                        this.paths[j].items = this.paths[j].items.filter(item => item.alive);  
+                    }
+
+                    this.paths = this.paths.filter(item => item.alive);  
+                         
+                    this.img = this.createImage();                 
+                });
+
+                //this.img = this.createImage();  
+            },
+            generatePath() {
+                let start = new V2(this.size.x/2, this.size.y/2);
                 
-                this.pathDots = mathUtils.getCurvePoints({start: this.start, end: this.end, midPoints: this.midPoints});    
+                let angle = getRandom(0,360);
+                let end = new V2(this.size.x, this.size.y).substract(start).rotate(angle).add(start);
+                
+                let dir = start.direction(end);
+                start.add(dir.mul(getRandomInt(1,5)), true);
+
+                let mpCount = getRandomInt(1,3);
+                this.maxDistance = 40
+                let yChangeChange = easing.createProps(mpCount-1, getRandomInt(5, 10), getRandomInt(10*mpCount, 15*mpCount), 'linear', 'base');
+                let midPoints = []//new Array(mpCount).fill().map(_ => getRandomInt(2,this.maxDistance-2)).sort((a,b)=> (a-b)).map(value => ({ distance: value/this.maxDistance }));
+                this.segWidth = 1/mpCount;
+                //console.log(midPoints)
+                let direction = -1;
+                if(mpCount > 1){
+                    for(let i = 0; i < mpCount; i++){
+                        yChangeChange.time = i;
+                        let yChange = easing.process(yChangeChange);
+                        let distance = getRandom(this.segWidth*i, this.segWidth*(i+1) - this.segWidth/2);
+                        if(distance < 0.05){
+                            distance = 0.05;
+                        }
+
+                        midPoints[i] = {
+                            yChange: yChange*direction,
+                            distance
+                        }
+                        direction*=-1;
+                    }
+                }
+                else {
+                    midPoints[0] = {
+                        distance: getRandomInt(this.maxDistance/3,this.maxDistance*2/3)/this.maxDistance,
+                        yChange: getRandomInt(10, 50)*(getRandomBool() ? 1: -1)
+                    };
+                }
+                
+                
+                this.pathDots = mathUtils.getCurvePoints({start: start, end: end, midPoints: midPoints});    
                 this.points = [];
                 createCanvas(new V2(1,1), (ctx) => {
                     let pp = new PerfectPixel({context: ctx});
@@ -35,64 +151,40 @@ class DollarScene extends Scene {
                     this.points = distinct(this.points, (p) => p.x+'_'+p.y);
                 })
 
-                this.items = [];
+                let reverse =  getRandomBool();
+                return  {
+                    points: this.points,
+                    items: [],
+                    alive: true,
+                    maxItems: getRandomInt(50, 300),
+                    itemsCreated: 0,
+                    colorH: reverse ? getRandomInt(0, 50) : getRandomInt(180, 230),
+                    reverse
+                };
+            },
+            getHsv(h,s,v){
+                let key = h+'_' + s+ '_' +v;
+                if(!this.hsvCache[key]){
+                    this.hsvCache[key] = colors.hsvToHex([h,s,v]);
+                }
 
-                this.angle = 0;
-                this.originPoints = [...this.points];
-                this.pathChangeTimer = this.regTimerDefault(15, () => {
-                    this.points = this.originPoints.map(op => new V2(op).substract(this.start).rotate(this.angle).add(this.start).toInt())
-                    this.angle+=1;
-                    if(this.angle > 360){
-                        this.angle-=360;
-                    }
-                })
-
-                this.itemsGeneratorTimer = this.regTimerDefault(30, () => {
-                    let time = fast.r(this.points.length*0.5);
-                    this.items.push({
-                        indexChange: easing.createProps(time, this.points.length-1, 0, 'quad', 'out'),
-                        xShiftChange: easing.createProps(time, getRandomInt(10,50), 1, 'quad', 'out'),
-                        alive: true
-                    })
-                });
-
-                this.itemsProcesserTimer = this.regTimerDefault(15, () => {
-                    for(let i = 0; i < this.items.length; i++){
-                        let item = this.items[i];
-                        if(item.index == 0){
-                            item.alive = false;
-                            continue;
-                        }
-
-                        // item.index = easing.process(item.indexChange);
-                        // item.xShift = easing.process(item.xShiftChange);                            
-                        easing.commonProcess({context: item, targetpropertyName: 'index', propsName: 'indexChange', round: true})
-                        easing.commonProcess({context: item, targetpropertyName: 'xShift', propsName: 'xShiftChange', round: true})
-
-                    }
-                    this.items = this.items.filter(item => item.alive);       
-
-                    this.img = this.createImage();                 
-                });
+                return this.hsvCache[key];
+                //return colors.hsvToHex([h,s,v]);
             },
             createImage() {
-                // if(!this.pathImg){
-                //     this.pathImg= createCanvas(this.size, (ctx, size, hlp) => {
-                //         hlp.setFillColor('gray');
-                //         for(let i = 0; i < this.points.length;i++){
-                //             hlp.dot(this.points[i].x, this.points[i].y)
-                //         }
-                //     })
-                // }
                 return createCanvas(this.size, (ctx, size, hlp) => {
-                    //let dots = mathUtils.getCurvePoints({start: this.start, end: this.end, midPoints: this.midPoints});    
-                    //ctx.drawImage(this.pathImg, 0,0)
+                    //let dots = mathUtils.getCurvePoints({start: this.start, end: this.end, midPoints: midPoints});    
+                    if(this.pathImg)
+                        ctx.drawImage(this.pathImg, 0,0)
 
-                    for(let i = 0; i < this.items.length; i++){
-                        let item = this.items[i];
-                        let pathDot = this.points[item.index];
-                        hlp.setFillColor('red').dot(pathDot.x + item.xShift, pathDot.y)
+                    for(let j = 0; j < this.paths.length; j++){
+                        for(let i = 0; i < this.paths[j].items.length; i++){
+                            let item = this.paths[j].items[i];
+                            let pathDot = this.paths[j].points[item.index];
+                            hlp.setFillColor(this.getHsv(this.paths[j].colorH, (item.isWhite ? 0 : item.s), item.v)).rect(pathDot.x + item.xShift, pathDot.y+item.yShift, item.size, item.size)
+                        }
                     }
+                    
                 })
             }
         }))
