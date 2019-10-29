ca2663b8e17500d360269cf15a3dd7646bc1c018
Starykh Aleksey <astarikh@mail.ru>
2019-10-29 17:18:18 +0300

    verse1 part 2 update

--- commission/epska/scenes/verse1_part2.js
+++ commission/epska/scenes/verse1_part2.js
@@ -15,6 +15,30 @@ class EpskaVerse1Part2Scene extends Scene {
     }
 
     start(){
-        //
+        this.eyes = this.addGo(new GO({
+            position: new V2(this.sceneCenter.x, 51),
+            size: new V2(300, 103),
+            init() {
+                // this.img = createCanvas(this.size, (ctx, size, hlp) => {
+                //     hlp.setFillColor('red').rect(0,0,size.x, size.y)
+                // })
+                this.base = this.addChild(new Go({
+                    position: new V2(),
+                    size: this.size.clone(),
+                    img: PP.createImage(SCG.epskaImageModels.verse1.eyesClose)
+                }))
+            }
+        }))
+
+        this.frames = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            init() {
+                this.img = createCanvas(this.size, (ctx, size, hlp) => {
+                    hlp.setFillColor('cornflowerblue').strokeRect(0,0,size.x, size.y,4)
+                    .rect(0,size.y/2, size.x, 4)
+                })
+            }
+        }), 2)
     }
 }
