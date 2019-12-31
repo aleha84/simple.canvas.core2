fb32719956071f3145a02301ed678a01cc1f8b25
Starykh Aleksey <astarikh@mail.ru>
2019-12-31 14:59:33 +0300

    midfight model update

--- demo9/scenes/midfight.js
+++ demo9/scenes/midfight.js
@@ -11,10 +11,34 @@ class Demo9MidFightScene extends Scene {
     }
 
     backgroundRender() {
-        this.backgroundRenderDefault();
+        this.backgroundRenderDefault('#C6980B');
     }
 
     start(){
-        //
+        this.knightSize = new V2(40,25);
+
+        this.knight = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.knightSize,
+            init() {
+                this.img = PP.createImage(Demo9MidFightScene.models.knightIdle)
+            }
+        }), 1)
+
+        this.grass = this.addGo(new GO({
+            position: this.sceneCenter.add(new V2(20,20)),
+            size: new V2(20,20),
+            init() {
+                this.img = PP.createImage(Demo9MidFightScene.models.grass)
+            }
+        }), 2)
+
+        this.grass2 = this.addGo(new GO({
+            position: this.sceneCenter.add(new V2(-20,20)),
+            size: new V2(20,20),
+            init() {
+                this.img = PP.createImage(Demo9MidFightScene.models.grass2)
+            }
+        }), 2)
     }
 }
