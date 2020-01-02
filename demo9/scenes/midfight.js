79b275229f553a4fa9279b39c8ca8af0da65906b
aleha84 <astarikh@mail.ru>
2020-01-03 00:14:06 +0300

    midfight update

--- demo9/scenes/midfight.js
+++ demo9/scenes/midfight.js
@@ -43,66 +43,106 @@ class Demo9MidFightScene extends Scene {
         //     }
         // }), 2)
 
-        this.grass2Img = PP.createImage(Demo9MidFightScene.models.grass2);
+        // this.grass2Img = PP.createImage(Demo9MidFightScene.models.grass2);
+        // this.addGo(new GO({
+        //     position: new V2(100, 180),
+        //     size: new V2(30,20),
+        //     init() {
+        //         this.img = this.parentScene.grass2Img;
+        //     }
+        // }), 2)
+
+        // this.addGo(new GO({
+        //     position: new V2(80, 190),
+        //     size: new V2(30,20),
+        //     init() {
+        //         this.img = PP.createImage(Demo9MidFightScene.models.grass3);
+        //     }
+        // }), 2)
+
         this.addGo(new GO({
-            position: new V2(100, 180),
-            size: new V2(30,20),
+            position: new V2(40, 80),
+            size: new V2(25,20),
             init() {
-                this.img = this.parentScene.grass2Img;
+                this.img = PP.createImage(Demo9MidFightScene.models.rock1);
             }
         }), 2)
 
         this.addGo(new GO({
-            position: new V2(80, 190),
-            size: new V2(30,20),
+            position: new V2(150, 120),
+            size: new V2(25,20),
             init() {
-                this.img = PP.createImage(Demo9MidFightScene.models.grass3);
-            }
-        }), 2)
-
-        this.addGo(new GO({
-            position: new V2(110, 210),
-            size: new V2(30,20),
-            init() {
-                this.img = PP.createImage(Demo9MidFightScene.models.grass4);
+                this.img = PP.createImage(Demo9MidFightScene.models.rock2);
             }
         }), 2)
         
+        // this.addGo(new GO({
+        //     position: new V2(110, 210),
+        //     size: new V2(30,20),
+        //     init() {
+        //         this.img = PP.createImage(Demo9MidFightScene.models.grass4);
+        //     }
+        // }), 2)
 
         this.grassItemsImg = Demo9MidFightScene.models.grassItems.map(model => PP.createImage(model));
         this.grassItemSize = new V2(19,15)
 
-        // for (let j = 0; j < 2; j++) 
-        // for (let i = 0; i < 11; i++) {
-        //     this.addGo(new GO({
-        //         position: new V2(this.grassItemSize.x*i + (j%2 == 0? 2: 0),250-j*15),
-        //         size: this.grassItemSize,
-        //         init() {
-        //             this.img = this.parentScene.grassItemsImg[0]
-        //             //getRandomInt(0, this.parentScene.grassItemsImg.lenght-1)
-        //         }
-        //     }), 3)
-            
-        // }
+     
 
-        this.addGo(new GO({
-            position: new V2(100,250),
-            size: new V2(200,100),
-            init() {
-                this.img = createCanvas(this.size, (ctx, size, hlp) => {
-                    let h = 16;
-                    let cy = 0;
-                    let xShift = false;
-                    for(let y = 0; y < size.y; y+=3){
-                        for(let x = 0; x < fast.r(size.x/this.parentScene.grassItemSize.x); x++){
-                            ctx.drawImage(this.parentScene.grassItemsImg[getRandomInt(0, this.parentScene.grassItemsImg.length-1)],
-                             this.parentScene.grassItemSize.x*x + (xShift ? 2 : 0), y) //(xShift ? fast.r(-this.parentScene.grassItemSize.x/2) : 0)
-                        }
+        // this.addGo(new GO({
+        //     position: new V2(100,250),
+        //     size: new V2(200,100),
+        //     init() {
+        //         this.img = createCanvas(this.size, (ctx, size, hlp) => {
+        //             let h = 16;
+        //             let cy = 0;
+        //             let xShift = false;
+        //             for(let y = 0; y < size.y; y+=3){
+        //                 for(let x = 0; x < fast.r(size.x/this.parentScene.grassItemSize.x); x++){
+        //                     ctx.drawImage(this.parentScene.grassItemsImg[getRandomInt(0, this.parentScene.grassItemsImg.length-1)],
+        //                      this.parentScene.grassItemSize.x*x + (xShift ? 2 : 0), y) //(xShift ? fast.r(-this.parentScene.grassItemSize.x/2) : 0)
+        //                 }
 
-                        xShift=!xShift;
+        //                 xShift=!xShift;
+        //             }
+        //         })
+        //     }
+        // }), 3)
+
+        let xShift = false;
+        for(let y = 220; y < this.viewport.y; y+=3){
+            for(let x = 0; x < fast.r(this.viewport.x/this.grassItemSize.x); x++){
+
+                this.addGo(new GO({
+                    position: new V2(this.grassItemSize.x*x + (xShift ? 2 : 0),y),
+                    size: this.grassItemSize,
+                    imgFrames: this.grassItemsImg[getRandomInt(0, this.grassItemsImg.length-1)],
+                    xShift,
+                    init() {
+                        this.currentFrame = 0;
+                        this.img = isArray(this.imgFrames) ? this.imgFrames[this.currentFrame] : this.imgFrames
+
+                        if(isArray(this.imgFrames))
+                            this.shake();
+                    },
+                    shake() {
+                        this.timer = this.regTimerDefault(200, () => {
+                            this.currentFrame++;
+                            if(this.currentFrame == this.imgFrames.length){
+                                this.currentFrame = 0;
+                            }
+
+                            this.img = this.imgFrames[this.currentFrame];
+                        }) 
+                        
                     }
-                })
+                }), y)
+
+                // ctx.drawImage(this.grassItemsImg[getRandomInt(0, this.grassItemsImg.length-1)],
+                //     this.grassItemSize.x*x + (xShift ? 2 : 0), y) 
             }
-        }), 3)
+
+            xShift=!xShift;
+        }
     }
 }
