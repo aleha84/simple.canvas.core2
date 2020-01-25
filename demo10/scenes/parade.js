03c5cc8918175f8211ffeb448e3aca7b0177f513
aleha84 <astarikh@mail.ru>
2020-01-26 01:45:11 +0300

    demo10 added

---
+++ demo10/scenes/parade.js
@@ -0,0 +1,20 @@
+class Demo10ParadeScene extends Scene {
+    constructor(options = {}) {
+        options = assignDeep({}, {
+            debug: {
+                enabled: true,
+                showFrameTimeLeft: true,
+                additional: [],
+            },
+        }, options)
+        super(options);
+    }
+
+    backgroundRender() {
+        this.backgroundRenderDefault('#DFE6F0');
+    }
+
+    start(){
+        //
+    }
+}
