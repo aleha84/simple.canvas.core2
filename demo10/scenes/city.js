6de8d1b8731f20fe03185bed8b17a6fb4ef6d629
Starykh Aleksey <astarikh@mail.ru>
2020-02-07 18:43:43 +0300

    demo10 city update

--- demo10/scenes/city.js
+++ demo10/scenes/city.js
@@ -14,7 +14,7 @@ class Demo10CityScene extends Scene {
         this.backgroundRenderDefault();
     }
 
-    flowGenerator({framesCount, itemsCount, from, to, path, upperY, fromShift= 0, lengthFrom = 0}) {
+    flowGenerator({framesCount, itemsCount, from, to, path, upperY, fromShift= 0, lengthFrom = 0, forceSingleFrame = false}) {
         let items = []
         for(let i = 0; i < itemsCount; i++){
             items[i] = {
@@ -59,7 +59,7 @@ class Demo10CityScene extends Scene {
 
         let result = [];
 
-        for(let f = 0; f < framesCount; f++){
+        for(let f = 0; f < (forceSingleFrame ? 1 : framesCount); f++){
             result[f] = createCanvas(this.viewport, (ctx, size, hlp) => {
                 for(let i = 0; i < itemsCount; i++){
                     let item = items[i];
@@ -81,13 +81,36 @@ class Demo10CityScene extends Scene {
                         let y = fast.r(p.y)
 
                         hlp.setFillColor(item.color).dot(x, y);
+                        hlp.setFillColor('rgba(0,0,0,0.1)').dot(x+1, y);
 
                         if(lengthFrom && item.lengths[currentPIndex] > 0) {
-                            for(let l = 0; l < item.lengths[currentPIndex]; l++){
-                                let p = item.linePoints[lineIndex-(l+1)];
-                                if(p != undefined)
-                                    hlp.setFillColor(item.color).dot(p.x, p.y)
+                            if( item.lengths[currentPIndex] == 1){
+                                hlp.setFillColor(item.color).dot(x-1, y+1)
+                                hlp.setFillColor('rgba(0,0,0,0.1)').dot(x, y+1)
+                                hlp.setFillColor('rgba(0,0,0,0.2)').dot(x-1, y+1)
+
+                                ctx.globalAlpha = 0.6;
+                                hlp.setFillColor(item.color).dot(x-1, y);
+                                ctx.globalAlpha = 1;
                             }
+                            if( item.lengths[currentPIndex] == 2){
+                                hlp.setFillColor(item.color).dot(x-1, y+1).dot(x-2, y+2)
+
+                                hlp.setFillColor('rgba(0,0,0,0.1)').dot(x, y+1).dot(x-1, y+2).dot(x+1, y-1);
+                                hlp.setFillColor('rgba(0,0,0,0.2)').dot(x-2, y+2)
+
+                                ctx.globalAlpha = 0.6;
+                                hlp.setFillColor(item.color).dot(x-1, y).dot(x-2, y+1).dot(x, y-1);
+                                ctx.globalAlpha = 1;
+                            }
+
+                           
+                            // for(let l = 0; l < item.lengths[currentPIndex]; l++){
+                            //     let p = item.linePoints[lineIndex-(l+1)];
+                            //     if(p != undefined)
+                            //         hlp.setFillColor(item.color).dot(p.x, p.y)
+                            // }
+
                         }
                     }
                 }
@@ -102,13 +125,20 @@ class Demo10CityScene extends Scene {
         this.layers = {
             overMainRoadBuilding: 21,
             mainRoad: 20,
-            mainRightBuilding: 19
+            mainRightBuilding: 19,
+            leftBuildings: 15,
+            centralBuildings: 17,
         }
 
-        this.flowColors = [];
-        for(let i = 0; i < 10; i++){
-            this.flowColors[i] = hsvToHex({hsv: {h: getRandomInt(0, 359), s: getRandomInt(20, 70), v: getRandomInt(50, 90)}, hsvAsObject: true})
-        }
+        this.flowAnimationDisabled = true;
+        let scene = this;
+
+        this.flowColors = [
+            '#A25156','#858F98','#98635E','#987C6C','#909FA1','#7F6154','#C1A99F','#7E92A9'
+        ];
+        // for(let i = 0; i < 10; i++){
+        //     this.flowColors[i] = hsvToHex({hsv: {h: getRandomInt(0, 359), s: getRandomInt(20, 70), v: getRandomInt(50, 90)}, hsvAsObject: true})
+        // }
 
         this.mainRoad = this.addGo(new GO({
             position: this.sceneCenter.clone(),
@@ -124,13 +154,12 @@ class Demo10CityScene extends Scene {
                     position: new V2(),
                     size: this.size,
                     frames: this.parentScene.flowGenerator({framesCount: 800, itemsCount: 250, from: new V2(64,207), to: new V2(200,54), upperY: 66, fromShift: 4,
-                    lengthFrom: 2}),
+                    lengthFrom: 2, forceSingleFrame: scene.flowAnimationDisabled}),
                     init() {
                         this.currentFrame = 0;
                         this.img = this.frames[this.currentFrame];
-        
                         this.timer = this.regTimerDefault(15, () => {
-            
+        
                             this.img = this.frames[this.currentFrame];
                             this.currentFrame++;
                             if(this.currentFrame == this.frames.length){
@@ -144,13 +173,13 @@ class Demo10CityScene extends Scene {
                     position: new V2(),
                     size: this.size,
                     frames: this.parentScene.flowGenerator({framesCount: 800, itemsCount: 250, from: new V2(48,207), to: new V2(199,49), upperY: 66, fromShift: 4,
-                        lengthFrom: 2}),
+                        lengthFrom: 2, forceSingleFrame: scene.flowAnimationDisabled}),
                     init() {
                         this.currentFrame = 0;
                         this.img = this.frames[this.currentFrame];
         
                         this.timer = this.regTimerDefault(15, () => {
-            
+        
                             this.img = this.frames[this.currentFrame];
                             this.currentFrame--;
                             if(this.currentFrame < 0){
@@ -185,5 +214,29 @@ class Demo10CityScene extends Scene {
                 }))
             }
         }), this.layers.overMainRoadBuilding)
+
+        this.leftBuildings = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            init() {
+                this.main = this.addChild(new GO({
+                    position: new V2(),
+                    size: this.size,
+                    img: PP.createImage(Demo10CityScene.models.leftBuildings)
+                }))
+            }
+        }), this.layers.leftBuildings)
+
+        this.centralBuildings = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            init() {
+                this.main = this.addChild(new GO({
+                    position: new V2(),
+                    size: this.size,
+                    img: PP.createImage(Demo10CityScene.models.centralBuildings)
+                }))
+            }
+        }), this.layers.centralBuildings)
     }
 }
