0f02d0bb5632f3cf8aef7f2c617c3ed2a2f9d801
aleha84 <astarikh@mail.ru>
2020-03-09 00:21:34 +0300

    pdaily bandana added

--- demo10/scenes/pDaily.js
+++ demo10/scenes/pDaily.js
@@ -2,7 +2,7 @@ class Demo10PDailyScene extends Scene {
     constructor(options = {}) {
         options = assignDeep({}, {
             debug: {
-                enabled: true,
+                enabled: false,
                 showFrameTimeLeft: true,
                 additional: [],
             },
@@ -11,68 +11,81 @@ class Demo10PDailyScene extends Scene {
     }
 
     backgroundRender() {
-        this.backgroundRenderDefault('#E7E5E8');
+        this.backgroundRenderDefault();
     }
 
     start(){
-        let frames = PP.createImage(pDailyModels.robothand);
+        this.bg = this.addGo(new GO({
+            position: this.sceneCenter,
+            size: this.viewport,
+            init() {
+                this.img = createCanvas(this.size, (ctx, size, hlp) => {
+                    hlp.setFillColor('black').rect(0,0,size.x, size.y)
+                })
+            }
+        }), 1)
+
+        //let frames = PP.createImage(pDailyModels.robothand);
+        let img = PP.createImage(pDailyModels.bandana);
 
         this.small = this.addGo(new GO({
             frames,
             position: new V2(50, 100),
-            size: new V2(25,25),
+            size: new V2(50,50),
+            img,
             init() {
-                this.currentFrame = 0;
-                this.img = this.frames[this.currentFrame];
-                this.redFrameCounter = 5;
+                // this.currentFrame = 0;
+                // this.img = this.frames[this.currentFrame];
+                // this.redFrameCounter = 5;
 
-                this.timer = this.regTimerDefault(50, () => {
+                // this.timer = this.regTimerDefault(50, () => {
     
-                    this.img = this.frames[this.currentFrame];
-                    this.currentFrame++;
-                    if(this.currentFrame == this.frames.length){
-                        this.currentFrame = 0;
+                //     this.img = this.frames[this.currentFrame];
+                //     this.currentFrame++;
+                //     if(this.currentFrame == this.frames.length){
+                //         this.currentFrame = 0;
 
-                        this.redFrameCounter--;
+                //         this.redFrameCounter--;
 
-                        if(this.redFrameCounter == 0){
-                            this.redFrameCounter= 5;
-                            if(!this.redFrame){
-                                this.redFrame = this.addChild(new GO({
-                                    position: new V2(),
-                                    size: this.size,
-                                    img: createCanvas(this.size, (ctx, size, hlp) => {
-                                        hlp.setFillColor('red').strokeRect(-5,-5, size.x, size.x)
-                                    })
-                                }));
-                            }
-                            else {
-                                this.removeChild(this.redFrame);
-                                this.redFrame = undefined;
-                            }
-                        } 
-                    }
-                })
+                //         if(this.redFrameCounter == 0){
+                //             this.redFrameCounter= 5;
+                //             if(!this.redFrame){
+                //                 this.redFrame = this.addChild(new GO({
+                //                     position: new V2(),
+                //                     size: this.size,
+                //                     img: createCanvas(this.size, (ctx, size, hlp) => {
+                //                         hlp.setFillColor('red').strokeRect(-5,-5, size.x, size.x)
+                //                     })
+                //                 }));
+                //             }
+                //             else {
+                //                 this.removeChild(this.redFrame);
+                //                 this.redFrame = undefined;
+                //             }
+                //         } 
+                //     }
+                // })
             }
-        }))
+        }),2)
 
         this.big = this.addGo(new GO({
             frames,
-            position: new V2(110, 100),
-            size: new V2(75,75),
+            position: new V2(120, 100),
+            size: new V2(120,120),
+            img,
             init() {
-                this.currentFrame = 0;
-                this.img = this.frames[this.currentFrame];
+                // this.currentFrame = 0;
+                // this.img = this.frames[this.currentFrame];
 
-                this.timer = this.regTimerDefault(50, () => {
+                // this.timer = this.regTimerDefault(50, () => {
     
-                    this.img = this.frames[this.currentFrame];
-                    this.currentFrame++;
-                    if(this.currentFrame == this.frames.length){
-                        this.currentFrame = 0;
-                    }
-                })
+                //     this.img = this.frames[this.currentFrame];
+                //     this.currentFrame++;
+                //     if(this.currentFrame == this.frames.length){
+                //         this.currentFrame = 0;
+                //     }
+                // })
             }
-        }))
+        }),2)
     }
 }
