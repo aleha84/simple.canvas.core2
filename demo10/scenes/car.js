81ae4b9fbf70aa9e2c59cb06c64a9aa943db7f72
aleha84 <astarikh@mail.ru>
2020-03-26 18:36:14 +0300

    demo10 car sceme created

---
+++ demo10/scenes/car.js
@@ -0,0 +1,20 @@
+class Demo10CarScene extends Scene {
+    constructor(options = {}) {
+        options = assignDeep({}, {
+            debug: {
+                enabled: true,
+                showFrameTimeLeft: true,
+                additional: [],
+            },
+        }, options)
+        super(options);
+    }
+
+    backgroundRender() {
+        this.backgroundRenderDefault();
+    }
+
+    start(){
+        //
+    }
+}
