965c797f4b666bcfe61c8cb35541b9077286f187
aleha84 <astarikh@mail.ru>
2020-05-14 20:09:14 +0300

    tea scene completed

--- demo10/scenes/tea.js
+++ demo10/scenes/tea.js
@@ -2,7 +2,7 @@ class Demo10TeaScene extends Scene {
     constructor(options = {}) {
         options = assignDeep({}, {
             debug: {
-                enabled: true,
+                enabled: false,
                 showFrameTimeLeft: true,
                 additional: [],
             },
@@ -26,12 +26,60 @@ class Demo10TeaScene extends Scene {
             size: this.viewport.clone(),
             frames: PP.createImage(Demo10TeaScene.models.spoonFrames),
             init() {
+                this.redFramesCounter = 6;
                 this.frames = [
                     this.frames[0],
                     ...this.frames,
                     this.frames[this.frames.length-1],
                     ...this.frames.reverse()
                 ]
+
+                console.log(this.frames.length);
+                this.currentFrame = 0;
+                this.framesDelayDefault = 10;
+                this.frameChangeDelay = this.framesDelayDefault;
+                this.img = this.frames[this.currentFrame];
+                
+                this.timer = this.regTimerDefault(15, () => {
+                    if(this.frameChangeDelay == 0){
+                        this.img = this.frames[this.currentFrame];
+                        this.currentFrame++;
+                        if(this.currentFrame == this.frames.length){
+                            this.currentFrame = 0;
+
+                            this.redFramesCounter--;
+
+                            if(this.redFramesCounter == 0){
+                                this.redFramesCounter = 6;
+                                if(!this.redFrame){
+                                                this.redFrame = this.addChild(new GO({
+                                                    position: new V2(),
+                                                    size: this.size,
+                                                    img: createCanvas(this.size, (ctx, size, hlp) => {
+                                                        hlp.setFillColor('red').rect(0,0, 50,50)
+                                                    })
+                                                }));
+                                            }
+                                            else {
+                                                this.removeChild(this.redFrame);
+                                                this.redFrame = undefined;
+                                            }
+                            }
+                        }
+                        this.frameChangeDelay = this.framesDelayDefault;
+                    }
+                    
+                    this.frameChangeDelay--;
+                })
+            }
+        }), 2)
+
+        this.spoon = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            frames: PP.createImage(Demo10TeaScene.models.steamFrames),
+            init() {
+                console.log(this.frames.length);
 
                 this.currentFrame = 0;
                 this.framesDelayDefault = 10;
@@ -51,6 +99,6 @@ class Demo10TeaScene extends Scene {
                     this.frameChangeDelay--;
                 })
             }
-        }), 2)
+        }), 3)
     }
 }
