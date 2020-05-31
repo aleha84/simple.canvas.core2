4d3c96897ff0660a2b2ccb5b1634dbab1462eab9
aleha84 <astarikh@mail.ru>
2020-05-31 03:19:29 +0300

    drive scene update

--- demo10/scenes/drive.js
+++ demo10/scenes/drive.js
@@ -21,6 +21,7 @@ class Demo10DriveScene extends Scene {
         let direction = this.pCenter.direction(new V2(0,82));
 
         let sharedPP = undefined;
+        let alphaMaskValues = easing.fast({from: 0.95, to: 0, steps: fast.r(framesPerModel*4/5), type: 'quad', method: eMethod}).map(v => fast.r(v, 2));
         
         createCanvas(new V2(1,1), (ctx, size, hlp) => {
             sharedPP = new PP({ctx});
@@ -40,54 +41,73 @@ class Demo10DriveScene extends Scene {
             model.sizeYValue = easing.fast({from: 1, to: modelSize.y, steps: framesPerModel, type: eType, method: eMethod}).map(v => fast.r(v));
 
             let modelCenter = new V2(0,0);//= modelSize.divide(2).toInt();
-
+            if(!model.frames){
+                model.frames = [];
+            }
             // let p2 = raySegmentIntersectionVector2(this.pCenter.add(modelCenter), direction, createLine(new V2(0, 0), new V2(0, size.y))).toInt().substract(modelSize);
 
              let movingLinePoints = sharedPP.lineV2(this.pCenter, targetPoint);
              let movingLineIndexValues = easing.fast({from: 0, to: movingLinePoints.length-1, steps: framesPerModel, type: eType, method: eMethod}).map(v => fast.r(v));
 
             for(let f = 0; f < framesPerModel; f++){
-                
-                
+                //let buildingFrame = undefined;
                 let frameIndex = f + modelsData[i].initialFrame;
                 if(frameIndex > (framesCount-1)){
                     frameIndex-=framesCount;
                 }
 
-                let frameModel  = {
-                    general: model.general,
-                    main: {
-                        layers: [
-                            {
-                                visible: true,
-                                order: 0,
-                                groups: []
+                if(!model.frames[f]){
+                    
+    
+                    let frameModel  = {
+                        general: model.general,
+                        main: {
+                            layers: [
+                                {
+                                    visible: true,
+                                    order: 0,
+                                    groups: []
+                                }
+                            ]
+                        }
+                    }
+    
+                    frameModel.general.size = new V2(model.sizeXValue[f], model.sizeYValue[f])
+    
+                    frameModel.main.layers[0].groups = model.main.layers[0].groups.map(originalGroup => {
+                        let group = assignDeep({}, originalGroup, {points: [] } );
+        
+                        group.points = originalGroup.points.map(originalPoint => {
+                            if(!originalPoint.lineDots){
+                                originalPoint.lineDots = sharedPP.lineV2(modelCenter, new V2(originalPoint.point));
+                                originalPoint.lineDotsIndexValues = easing.fast({from: 0, to: originalPoint.lineDots.length-1, steps: framesPerModel, type: eType, method: eMethod}).map(v => fast.r(v))
                             }
-                        ]
+        
+                            let dot = originalPoint.lineDots[originalPoint.lineDotsIndexValues[f]];
+                            let point = assignDeep({}, originalPoint, { point: dot, lineDots: undefined, lineDotsIndexValues: undefined })
+        
+                            return point;
+                        })
+                        
+                        return group;
+                    })
+
+                    model.frames[f] = {
+                        img:  createCanvas(frameModel.general.size, (ctx, size, hlp) => {
+                            ctx.drawImage(PP.createImage(frameModel), 0,0);
+                            ctx.globalCompositeOperation = 'source-atop';
+                            let a = alphaMaskValues[f];
+                            if(a == undefined)
+                                a = 0;
+                            hlp.setFillColor(`rgba(0,0,0,${a})`).rect(0,0,size.x, size.y);
+                        }) ,
+                        size: frameModel.general.size
                     }
                 }
-
-                frameModel.general.size = new V2(model.sizeXValue[f], model.sizeYValue[f])
-
-                frameModel.main.layers[0].groups = model.main.layers[0].groups.map(originalGroup => {
-                    let group = assignDeep({}, originalGroup, {points: [] } );
+                
+                
     
-                    group.points = originalGroup.points.map(originalPoint => {
-                        if(!originalPoint.lineDots){
-                            originalPoint.lineDots = sharedPP.lineV2(modelCenter, new V2(originalPoint.point));
-                            originalPoint.lineDotsIndexValues = easing.fast({from: 0, to: originalPoint.lineDots.length-1, steps: framesPerModel, type: eType, method: eMethod}).map(v => fast.r(v))
-                        }
-    
-                        let dot = originalPoint.lineDots[originalPoint.lineDotsIndexValues[f]];
-                        let point = assignDeep({}, originalPoint, { point: dot, lineDots: undefined, lineDotsIndexValues: undefined })
-    
-                        return point;
-                    })
-                    
-                    return group;
-                })
-    
-                let buildingFrame = PP.createImage(frameModel);
+                let buildingFrame = model.frames[f].img;//PP.createImage(frameModel);
                 let bPosition = new V2(movingLinePoints[movingLineIndexValues[f]]);//.substract(modelCenter).toInt();
 
                 if(!framesData[frameIndex]){
@@ -96,7 +116,7 @@ class Demo10DriveScene extends Scene {
 
                 let order = f;
                 framesData[frameIndex][order] = {
-                    modelSize: frameModel.general.size,
+                    modelSize: model.frames[f].size,//frameModel.general.size,
                     img: buildingFrame,
                     position: bPosition
                 }
@@ -124,7 +144,7 @@ class Demo10DriveScene extends Scene {
                             let { img, position, modelSize } = frameData[i]
                             //let bPosition = new V2(movingLinePoints[movingLineIndexValues[frameData[i].modelFrameIndex]]).substract(modelCenter).toInt();
                             ctx.drawImage(img, position.x - modelSize.x, position.y);  
-                            hlp.setFillColor('red').dot(position.x, position.y)
+                            //hlp.setFillColor('red').dot(position.x, position.y)
                         }
                     }
                 })
@@ -136,9 +156,16 @@ class Demo10DriveScene extends Scene {
     }
 
     start(){
-        this.pCenter = new V2(132, 62);
+        this.pCenter = new V2(149, 62);
 
         var model = Demo10DriveScene.models.main;
+
+        this.bg = this.addGo(new GO({
+            position: this.sceneCenter,
+            size: this.viewport,
+            img: PP.createImage(Demo10DriveScene.models.bg)
+        }), 0)
+
         for(let l = 0; l < model.main.layers.length; l++){
             let name = model.main.layers[l].name;
 
@@ -155,47 +182,23 @@ class Demo10DriveScene extends Scene {
             position: this.sceneCenter.clone(),
             size: this.viewport.clone(),
             img: PP.createImage(Demo10DriveScene.models.road)
-        }),1)
+        }),5)
 
         this.addGo(new GO({
             position: this.sceneCenter,
             size: this.viewport,
             init() {
                 this.frames = this.parentScene.createBuildingsFrames(
-                    {framesCount: 200, framesPerModel: 200, 
+                    {framesCount: 400, framesPerModel: 400, 
                         modelsData: [
                             {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 0,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 25,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 50,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 75,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
+                                model: Demo10DriveScene.models.buildings.b4,
                                 initialFrame: 100,
                             },
                             {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 125,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 150,
-                            },
-                            {
-                                model: Demo10DriveScene.models.buildings.b1,
-                                initialFrame: 175,
-                            },
+                                model: Demo10DriveScene.models.buildings.b4,
+                                initialFrame: 300,
+                            }
                         ], 
                         size: this.size, targetPoint: new V2(-150, this.parentScene.pCenter.y-50) })
 
@@ -211,6 +214,122 @@ class Demo10DriveScene extends Scene {
                     }
                 })
             }
-        }), 5)
+        }), 3)
+
+        this.addGo(new GO({
+            position: this.sceneCenter,
+            size: this.viewport,
+            init() {
+                this.frames = this.parentScene.createBuildingsFrames(
+                    {framesCount: 300, framesPerModel: 300, 
+                        modelsData: [
+                            {
+                                model: Demo10DriveScene.models.buildings.b3,
+                                initialFrame: 100,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b3,
+                                initialFrame: 200,
+                            }
+                        ], 
+                        size: this.size, targetPoint: new V2(-150, this.parentScene.pCenter.y-160) })
+
+                this.currentFrame = 0;
+                this.img = this.frames[this.currentFrame];
+                
+                this.timer = this.regTimerDefault(15, () => {
+                
+                    this.img = this.frames[this.currentFrame];
+                    this.currentFrame++;
+                    if(this.currentFrame == this.frames.length){
+                        this.currentFrame = 0;
+                    }
+                })
+            }
+        }), 4)
+
+        this.addGo(new GO({
+            position: this.sceneCenter,
+            size: this.viewport,
+            init() {
+                this.frames = this.parentScene.createBuildingsFrames(
+                    {framesCount: 200, framesPerModel: 200, 
+                        modelsData: [
+                            ...new Array(40).fill().map((_,i) => ({
+                                model: Demo10DriveScene.models.buildings.vLine,
+                                initialFrame: i*5,
+                            }))
+                        ], 
+                        size: this.size, targetPoint: new V2(-150, this.parentScene.pCenter.y-25) })
+
+                this.currentFrame = 0;
+                this.img = this.frames[this.currentFrame];
+                
+                this.timer = this.regTimerDefault(15, () => {
+                
+                    this.img = this.frames[this.currentFrame];
+                    this.currentFrame++;
+                    if(this.currentFrame == this.frames.length){
+                        this.currentFrame = 0;
+                    }
+                })
+            }
+        }), 6)
+
+        this.addGo(new GO({
+            position: this.sceneCenter,
+            size: this.viewport,
+            init() {
+                this.frames = this.parentScene.createBuildingsFrames(
+                    {framesCount: 200, framesPerModel: 200, 
+                        modelsData: [
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 0,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 25,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 50,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 75,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 100,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 125,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 150,
+                            },
+                            {
+                                model: Demo10DriveScene.models.buildings.b2,
+                                initialFrame: 175,
+                            },
+                        ], 
+                        size: this.size, targetPoint: new V2(-150, this.parentScene.pCenter.y-160) })
+
+                this.currentFrame = 0;
+                this.img = this.frames[this.currentFrame];
+                
+                this.timer = this.regTimerDefault(15, () => {
+                
+                    this.img = this.frames[this.currentFrame];
+                    this.currentFrame++;
+                    if(this.currentFrame == this.frames.length){
+                        this.currentFrame = 0;
+                    }
+                })
+            }
+        }), 6)
     }
 }
