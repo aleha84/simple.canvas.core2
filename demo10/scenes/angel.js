b7432253489554421ee2a22af9f6e1f8dfdb2410
aleha84 <astarikh@mail.ru>
2020-09-23 00:17:23 +0300

    Update angel.js

--- demo10/scenes/angel.js
+++ demo10/scenes/angel.js
@@ -28,11 +28,12 @@ class Demo10AngelScene extends Scene {
             ...easing.fast({from: this.sceneCenter.y, to: this.sceneCenter.y-2, steps: levitationFramesCount/2, type: 'quad', method: 'inOut', round: 0}),
             ...easing.fast({from: this.sceneCenter.y-2, to: this.sceneCenter.y, steps: levitationFramesCount/2, type: 'quad', method: 'inOut', round: 0})
         ]
+        let levitationEnabled = false;
 
         this.streams = this.addGo(new GO({
             position: this.sceneCenter.clone(),
             size: this.viewport.clone(),
-            createStreamFrames({framesCount, dotsData, size, xClamps, addX = 0, addY = 0}) {
+            createStreamFrames({framesCount, dotsData, size, xClamps, addX = 0, addY = 0, particleReverse= false}) {
                 let frames = [];
                 let _sharedPP;
                 let halfFramesCount = fast.r(framesCount/2);
@@ -54,14 +55,64 @@ class Demo10AngelScene extends Scene {
                     }
                 });
 
+                let framesData = [];
+                let particleData = [];
+                let particlesTotalFrames = 60;
+                let oValues = easing.fast({ from: 1, to: 0, steps: particlesTotalFrames, type: 'quad', method: 'in', round: 2});
                 
                 for(let f = 0; f < framesCount; f++){
+                    framesData[f] = {dots: []};
                     let dots = dotsData.map(dd => dd.dots[f]);
                     let formula = mathUtils.getCubicSplineFormula(dots);
+                    for(let x = xClamps[0]; x < xClamps[1]; x++){
+                        let y=  fast.r(formula(x));
+                        framesData[f].dots.push({x,y});
+                    }
+
+                    let particlesCount = getRandomInt(0,2);
+                    let startFrameIndex = f;
+                    let totalFrames = particlesTotalFrames;
+                    for(let i = 0; i < particlesCount; i++){
+                        let yShiftChange = new Array(particlesTotalFrames).fill(getRandomInt(0,2));
+                        if(getRandomInt(0,3) == 0)
+                            yShiftChange = easing.fast({ from: 0, to: -getRandomInt(5,15), steps: particlesTotalFrames, type: 'quad', method: 'in', round: 0});
+                        let data = { frames: []};
+                        let startDotIndex = getRandomInt(0, framesData[f].dots.length-1);
+                        let indexShift = -15;
+                        if(particleReverse){
+                            indexShift = 15;
+                        }
+                        let indexValues = easing.fast({from: startDotIndex, to: startDotIndex+indexShift, steps: totalFrames, type: 'quad', method: 'inOut', round: 0});
+                        for(let _f = 0; _f < totalFrames; _f++){
+                            let frameIndex = _f + startFrameIndex;
+                            if(frameIndex > (framesCount-1)){
+                                frameIndex-=framesCount;
+                            }
+
+                            let p = framesData[f].dots[indexValues[_f]];
+                            p = new V2(p);
+                            p.y += yShiftChange[_f];
+                            data.frames[frameIndex] = {p, o: oValues[_f]};
+                        }
+
+                        particleData.push(data);
+                    }
+                }
+
+                for(let f = 0; f < framesCount; f++){
                     frames[f] = createCanvas(size, (ctx, size, hlp) => {
-                        for(let x = xClamps[0]; x < xClamps[1]; x++){
-                            let y=  fast.r(formula(x));
-                            hlp.setFillColor('#cfccce').rect(x, y, 1 + addX, 1 + addY);
+                        for(let i = 0; i < framesData[f].dots.length; i++){
+                            hlp.setFillColor('#cfccce').rect(framesData[f].dots[i].x, framesData[f].dots[i].y, 1 + addX, 1 + addY);
+                        }
+
+                        for(let p = 0; p < particleData.length; p++){
+                            let itemData = particleData[p];
+                            
+                            if(itemData.frames[f]){
+                                if(itemData.frames[f].p)
+                                    hlp.setFillColor(`rgba(255,255,255, ${itemData.frames[f].o})`).dot(itemData.frames[f].p.x, itemData.frames[f].p.y);
+                            }
+                            
                         }
                     });
                 }
@@ -124,7 +175,38 @@ class Demo10AngelScene extends Scene {
                             { dots: [new V2(-20,181), new V2(-20,195)] }, 
                             { dots: [new V2(42, 81), new V2(42, 84)] }, 
                             { dots: [new V2(57, 67)] }
-                     ], size: this.size, xClamps: [-20, 67], addY: 2 })
+                     ], size: this.size, xClamps: [-20, 67], addY: 2 }),
+                     ////////////////////////////
+                     this.createStreamFrames({ framesCount: 200, 
+                        dotsData: [ 
+                            { dots: [new V2(128, 43)] },
+                            { dots: [new V2(163, 58), new V2(164,60)] }, 
+                            { dots: [new V2(220, 38), new V2(220, 43)] }
+                     ], size: this.size, xClamps: [128, 220], addY: 1, particleReverse: true }),
+                     this.createStreamFrames({ framesCount: 200, 
+                        dotsData: [ 
+                            { dots: [new V2(149, 69)] },
+                            { dots: [new V2(180, 74), new V2(180,77)] }, 
+                            { dots: [new V2(220, 72), new V2(220, 77)] }
+                     ], size: this.size, xClamps: [149, 220], addY: 1, particleReverse: true }),
+                     this.createStreamFrames({ framesCount: 200, 
+                        dotsData: [ 
+                            { dots: [new V2(149, 74)] },
+                            { dots: [new V2(178, 86), new V2(177,88)] }, 
+                            { dots: [new V2(220, 89), new V2(220, 95)] }
+                     ], size: this.size, xClamps: [128, 220], addY: 1, particleReverse: true }),
+                     this.createStreamFrames({ framesCount: 200, 
+                        dotsData: [ 
+                            { dots: [new V2(146, 75)] },
+                            { dots: [new V2(176, 93), new V2(177,95)] }, 
+                            { dots: [new V2(220, 120), new V2(220, 126)] }
+                     ], size: this.size, xClamps: [146, 220], addY: 1, particleReverse: true }),
+                     this.createStreamFrames({ framesCount: 200, 
+                        dotsData: [ 
+                            { dots: [new V2(142, 76)] },
+                            { dots: [new V2(172, 102), new V2(172,105)] }, 
+                            { dots: [new V2(220, 156), new V2(220, 163)] }
+                     ], size: this.size, xClamps: [142, 220], addY: 1, particleReverse: true })
                 ]
 
                 this.streamItems = framesData.map(frames => this.addChild(new GO({
@@ -162,17 +244,20 @@ class Demo10AngelScene extends Scene {
                     }
                 })))
 
-                let frameIndex = 0;
-                let totalFrames = levitationFramesCount;
-                this.timer = this.regTimerDefault(10, () => {
-                    let y = yChangeValues[frameIndex];
-                    this.position.y = y;
-                    this.needRecalcRenderProperties = true;
-                    frameIndex++;
-                    if(frameIndex == totalFrames){
-                        frameIndex = 0;
-                    }
-                })
+                if(levitationEnabled){
+                    let frameIndex = 0;
+                    let totalFrames = levitationFramesCount;
+                    this.timer = this.regTimerDefault(10, () => {
+                        let y = yChangeValues[frameIndex];
+                        this.position.y = y;
+                        this.needRecalcRenderProperties = true;
+                        frameIndex++;
+                        if(frameIndex == totalFrames){
+                            frameIndex = 0;
+                        }
+                    })
+                }
+                
             }
         }), 5)
 
@@ -182,17 +267,20 @@ class Demo10AngelScene extends Scene {
             init() {
                 this.img = PP.createImage(Demo10AngelScene.models.main, { exclude: ['bg'] })
 
-                let frameIndex = 0;
-                let totalFrames = levitationFramesCount;
-                this.timer = this.regTimerDefault(10, () => {
-                    let y = yChangeValues[frameIndex];
-                    this.position.y = y;
-                    this.needRecalcRenderProperties = true;
-                    frameIndex++;
-                    if(frameIndex == totalFrames){
-                        frameIndex = 0;
-                    }
-                })
+                if(levitationEnabled){
+                    let frameIndex = 0;
+                    let totalFrames = levitationFramesCount;
+                    this.timer = this.regTimerDefault(10, () => {
+                        let y = yChangeValues[frameIndex];
+                        this.position.y = y;
+                        this.needRecalcRenderProperties = true;
+                        frameIndex++;
+                        if(frameIndex == totalFrames){
+                            frameIndex = 0;
+                        }
+                    })
+                }
+                
             }
         }), 10)
     }
