4110bf1c6b6b4eb427a6a1c627bfae4eb0d113f5
aleha84 <astarikh@mail.ru>
2020-11-07 01:11:40 +0300

    update scene

--- commission/thetraviswaltons/scene.js
+++ commission/thetraviswaltons/scene.js
@@ -26,8 +26,10 @@ class DrunkDetectiveScene extends Scene {
         this.guy = this.addGo(new GO({
             position: this.sceneCenter.clone(),
             size: new V2(200, 113),
+            frames: PP.createImage(DrunkDetectiveScene.models.guy), 
             init() {
-                this.img = PP.createImage(DrunkDetectiveScene.models.guy)[0]
+                this.registerFramesDefaultTimer({originFrameChangeDelay: 10});
+                // this.img = PP.createImage(DrunkDetectiveScene.models.guy)[0]
             }
         }), 10)
     }
