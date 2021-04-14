70fe105f0ff98ad57e4e0bbd962c623dd571da21
Starykh Aleksey <astarikh@mail.ru>
2021-04-15 01:25:22 +0300

    update

--- art/scenes/turbo2/scene.js
+++ art/scenes/turbo2/scene.js
@@ -1,8 +1,18 @@
 class Turbo2Scene extends Scene {
     constructor(options = {}) {
         options = assignDeep({}, {
-            debug: {
+            capturing: {
                 enabled: true,
+                addRedFrame: false,
+                stopByCode: true,
+                //viewportSizeMultiplier: 7,
+                size: new V2(2000,1480),
+                totalFramesToRecord: 601,
+                frameRate: 60,
+                fileNamePrefix: 'turbo2'
+            },
+            debug: {
+                enabled: false,
                 showFrameTimeLeft: true,
                 additional: [],
             },
@@ -19,7 +29,7 @@ class Turbo2Scene extends Scene {
         let model = Turbo2Scene.models.main;
         let layersData = {};
         let exclude = [
-            'tree_p', 'island_p', 'island_p2', 'bushes_p'
+            'tree_p', 'island_p', 'island_p2', 'bushes_p', 'logo_shine_zone'
         ];
         
         for(let i = 0; i < model.main.layers.length; i++) {
@@ -41,7 +51,97 @@ class Turbo2Scene extends Scene {
             size: this.viewport.clone(),
             img: PP.createImage(model, { renderOnly: [layerName] }),
             init() {
-        
+                // if(layerName == 'logo') {
+                //     this.lettersAnimation = this.addChild(new GO({
+                //         position: new V2(0, -0.05),
+                //         size: this.size,
+                //         init() {
+                //             let colorsRgb = [[1,1,1], [0,0,0], [2,2,2], [54,206,214]]
+                //             let colorsRgbLeft = [[179,175,72], [135,132,55], [90,88,37], [151,176,158]]
+                //             let pRaw = getPixels(this.parent.img, this.size);
+                //             let pixels = [
+                //                 ...pRaw.filter(pd => colorsRgb.find(c => c[0] == pd.color[0] && c[1] == pd.color[1] && c[2] == pd.color[2])).map(p => {
+                //                     p.direction = 1;
+
+                //                     return p;
+                //                 }),
+                //                 ...pRaw.filter(pd => colorsRgbLeft.find(c => c[0] == pd.color[0] && c[1] == pd.color[1] && c[2] == pd.color[2])).map(p => {
+                //                     p.direction = -1;
+
+                //                     return p;
+                //                 })
+                //             ]
+                            
+                //             let createPixelsShiftFrames = function({framesCount, pixelsData, itemFrameslength, size}) {
+                //                 let frames = [];
+                                
+                //                 let minX = Math.min(...pixelsData.map(pd => pd.position.x))
+                //                 let maxX = Math.max(...pixelsData.map(pd => pd.position.x))
+
+                //                 let xShiftValues = easing.fast({from: 0, to: 1, steps: itemFrameslength, type: 'linear', round: 0});
+                //                 let xValueChange = easing.fast({from: minX-1, to: maxX+1, steps: framesCount, type: 'linear', round: 0});
+
+                //                 let itemsData = pixelsData.map((el, i) => {
+                //                     let startFrameIndex =  xValueChange.findIndex(x => x == el.position.x);//getRandomInt(0, framesCount-1);
+                //                     let totalFrames = itemFrameslength;
+                                
+                //                     let frames = [];
+                //                     for(let f = 0; f < totalFrames; f++){
+                //                         let frameIndex = f + startFrameIndex;
+                //                         if(frameIndex > (framesCount-1)){
+                //                             frameIndex-=framesCount;
+                //                         }
+                                
+                //                         frames[frameIndex] = {
+                //                             xShift: xShiftValues[f]
+                //                         };
+                //                     }
+                                
+                //                     return {
+                //                         pixelData: el,
+                //                         frames
+                //                     }
+                //                 })
+                                
+                //                 for(let f = 0; f < framesCount; f++){
+                //                     frames[f] = createCanvas(size, (ctx, size, hlp) => {
+                //                         for(let p = 0; p < itemsData.length; p++){
+                //                             let itemData = itemsData[p];
+                                            
+                //                             if(itemData.frames[f]){
+                //                                 let c = itemData.pixelData.color;
+                //                                 hlp.setFillColor(`rgba(${c[0]}, ${c[1]}, ${c[2]}, ${c[3]})`)
+                //                                     .dot(itemData.pixelData.position.x + itemData.frames[f].xShift*itemData.pixelData.direction, itemData.pixelData.position.y)
+                //                             }
+                                            
+                //                         }
+                //                     });
+                //                 }
+                                
+                //                 return frames;
+                //             }
+
+                //             this.frames = createPixelsShiftFrames({ framesCount: 150, pixelsData: pixels, itemFrameslength: 20, size: this.size })
+                //             this.registerFramesDefaultTimer({});
+                //         }
+                //     }))
+                // }
+
+                if(layerName == 'press_start') {
+                    this.currentFrame = 0;
+                    let totalFrames = 300;
+                    this.position.y-=2;
+                    this.timer = this.regTimerDefault(15, () => {
+                        this.currentFrame++;
+                        if(this.currentFrame == totalFrames){
+                            this.currentFrame = 0;
+                        }
+
+                        if(this.currentFrame % 30 == 0)
+                            this.isVisible = !this.isVisible; 
+                        
+                    })
+                }
             }
         }), renderIndex)
         
@@ -155,7 +255,11 @@ class Turbo2Scene extends Scene {
             init() {
                 this.frames = this.createSeaFrames({ framesCount: 300, itemsCount: 4000, itemFrameslength: 100, size: this.size })
 
-                this.registerFramesDefaultTimer({});
+                this.registerFramesDefaultTimer({
+                    framesEndCallback: () => { 
+                        this.parentScene.capturing.stop = true; 
+                    }
+                });
             }
         }), layersData.sea.renderIndex+1)
 
@@ -598,9 +702,9 @@ class Turbo2Scene extends Scene {
                     { layerName: 'l1', animationStartFrame: 0, },
                     { layerName: 'l2', animationStartFrame: 10, },
                     { layerName: 'l3', animationStartFrame: 20, },
-                    // { layerName: 'l4', animationStartFrame: 60, },
-                    // { layerName: 'l5', animationStartFrame: 70, },
-                    // { layerName: 'l6', animationStartFrame: 80, },
+                    { layerName: 'l4', animationStartFrame: 30, },
+                    { layerName: 'l5', animationStartFrame: 45, },
+                    { layerName: 'l6', animationStartFrame: 55, },
                 ]
 
                 this.animations = aniParams.map(p => this.addChild(new GO({
@@ -658,6 +762,109 @@ class Turbo2Scene extends Scene {
             }
         }), layersData.road.renderIndex+1)
 
-        //car
+        //logo
+        this.logo_particles = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            createParticlesFrames({framesCount, itemsCount, itemFrameslength, size}) {
+                let frames = [];
+                let startPoints = [];
+                createCanvas(new V2(1,1), (ctx, size, hlp) => {
+                    let corners = model.main.layers.find(l => l.name == 'logo_shine_zone').groups[0].points.map(p => new V2(p.point))
+
+                    startPoints = new PP({ctx}).fillByCornerPoints(corners);
+                })
+
+                let itemsData = new Array(itemsCount).fill().map((el, i) => {
+                    let startFrameIndex = getRandomInt(0, framesCount-1);
+                    let totalFrames = getRandomInt(itemFrameslength/2, itemFrameslength)//itemFrameslength + getRandomInt(-20,10);
+                
+                    let startP = startPoints[getRandomInt(0, startPoints.length-1)];
+                    //let yValues = easing.fast({ from: startP.y, to: startP.y-getRandomInt(10,25), steps: totalFrames, type: 'linear', method: 'base', round: 0  });
+                    let oValues = easing.fast({ from: 1, to: 0, steps: totalFrames, type: 'quad', method: 'out', round: 1  });
+
+                    let frames = [];
+                    let size = 1;
+                    let sizeValues = new Array(totalFrames).fill(size);
+                    if(getRandomBool){//getRandomBool()){
+                        let s = 2//getRandomInt(2,3);
+                        sizeValues = [
+                            ...easing.fast({ from: 1, to: s, steps: fast.r(totalFrames/2), type: 'quad', method: 'inOut', round: 0  }),
+                            ...easing.fast({ from: s, to: 1, steps: fast.r(totalFrames/2), type: 'quad', method: 'inOut', round: 0  }),
+                        ]
+                        //sizeValues = easing.fast({ from: 3, to: 1, steps: fast.r(totalFrames/2), type: 'quad', method: 'inOut', round: 0  })
+                    }
+                    for(let f = 0; f < totalFrames; f++){
+                        let frameIndex = f + startFrameIndex;
+                        if(frameIndex > (framesCount-1)){
+                            frameIndex-=framesCount;
+                        }
+                
+                        frames[frameIndex] = {
+                            size: sizeValues[f],
+                            y: startP.y,
+                            o: 1//oValues[f]
+                        };
+                    }
+
+                    let c = '#FFFFFF';
+                    // let c = '#9d8779';
+                    // if(getRandomInt(0,2) == 0){
+                    //     c = '#a66730';
+                    // }
+
+                    
+                
+                    return {
+                        c,
+                        x: startP.x,
+                        frames
+                    }
+                })
+                
+                for(let f = 0; f < framesCount; f++){
+                    frames[f] = createCanvas(size, (ctx, size, hlp) => {
+                        for(let p = 0; p < itemsData.length; p++){
+                            let itemData = itemsData[p];
+                            ctx.globalAlpha = 1;
+                            if(itemData.frames[f]){
+                                ctx.globalAlpha = itemData.frames[f].o;
+                                hlp.setFillColor(itemData.c)
+                                //
+                                // if(itemData.frames[f].size > 1){
+                                //     hlp.setFillColor('#CCA486')
+                                // }
+                                hlp.dot(itemData.x, itemData.frames[f].y)
+                                hlp.setFillColor(itemData.c)
+                                if(itemData.frames[f].size > 1){
+                                    ctx.globalAlpha = itemData.frames[f].o*2/3
+                                    hlp.dot(itemData.x-1, itemData.frames[f].y).dot(itemData.x+1, itemData.frames[f].y)
+                                    .dot(itemData.x, itemData.frames[f].y-1).dot(itemData.x, itemData.frames[f].y+1)
+                                }
+                                if(itemData.frames[f].size > 2){
+                                    ctx.globalAlpha = itemData.frames[f].o/3
+                                    hlp.dot(itemData.x-2, itemData.frames[f].y).dot(itemData.x+2, itemData.frames[f].y)
+                                    .dot(itemData.x, itemData.frames[f].y-2).dot(itemData.x, itemData.frames[f].y+2)
+                                    .dot(itemData.x-1, itemData.frames[f].y-1).dot(itemData.x-1, itemData.frames[f].y+1)
+                                    .dot(itemData.x+1, itemData.frames[f].y-1).dot(itemData.x+1, itemData.frames[f].y+1)
+                                }
+                                else {
+                                    
+                                }
+                            }
+                            
+                        }
+                    });
+                }
+                
+                return frames;
+            },
+            init() {
+                this.frames = this.createParticlesFrames({ framesCount: 300, itemsCount: 30, itemFrameslength: 70, size: this.size });
+                this.registerFramesDefaultTimer({});
+            }
+        }), layersData.logo.renderIndex+1)
+
+        //press start
     }
 }
