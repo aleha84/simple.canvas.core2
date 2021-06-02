1a5ae9bcced8b32621330bf42c7ccc2bbddc4425
Starykh Aleksey <astarikh@mail.ru>
2021-06-03 00:44:13 +0300

    editor update + new scene

---
+++ art/scenes/patterns/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function sceneSelectByHashValue(){
+        let sceneName = location.hash.replace('#','');
+        if(sceneName == ''){
+            return;
+        }
+        
+        if(SCG.scenes.cachedScenes[sceneName] != undefined)
+            SCG.scenes.selectScene(sceneName);
+        else 
+            return;
+    }
+
+    SCG.globals.version = 0.1;
+
+    SCG.src = {
+        
+    }
+
+    
+    SCG.scenes.cacheScene(new PatternsScene({
+        name:'patterns',
+        viewport: new V2(180,200)
+    }));
+
+    SCG.main.startV2('patterns');
+});
