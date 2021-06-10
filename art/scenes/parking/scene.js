297a8c58bef5f6aafe6828ef0185630d1261b043
Starykh Aleksey <astarikh@mail.ru>
2021-06-10 20:18:45 +0300

    update

---
+++ art/scenes/parking/scene.js
@@ -0,0 +1,50 @@
+class ParkingScene extends Scene {
+    constructor(options = {}) {
+        options = assignDeep({}, {
+            debug: {
+                enabled: true,
+                showFrameTimeLeft: true,
+                additional: [],
+            },
+        }, options)
+        super(options);
+    }
+
+    backgroundRender() {
+        this.backgroundRenderDefault();
+    }
+
+    start(){
+        let model = ParkingScene.models.main;
+        let layersData = {};
+        let exclude = [
+            
+        ];
+        
+        for(let i = 0; i < model.main.layers.length; i++) {
+            let layer = model.main.layers[i];
+            let layerName = layer.name || layer.id;
+            let renderIndex = i*10;
+        
+        layersData[layerName] = {
+            renderIndex
+        }
+        
+        if(exclude.indexOf(layerName) != -1){
+            console.log(`${layerName} - skipped`)
+            continue;
+        }
+        
+        this[layerName] = this.addGo(new GO({
+            position: this.sceneCenter.clone(),
+            size: this.viewport.clone(),
+            img: PP.createImage(model, { renderOnly: [layerName] }),
+            init() {
+        
+            }
+        }), renderIndex)
+        
+        console.log(`${layerName} - ${renderIndex}`)
+        }
+    }
+}
