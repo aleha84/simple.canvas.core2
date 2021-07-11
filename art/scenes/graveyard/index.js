f01e0530b8b8c31d9847d05ba832144d2360727f
Starykh Aleksey <astarikh@mail.ru>
2021-07-12 01:11:40 +0300

    update

---
+++ art/scenes/graveyard/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function sceneSelectByHashValue(){
+        let sceneName = location.hash.replace('#','');
+        if(sceneName == ''){
+            return;
+        }
+        
+        if(SCG.scenes.cachedScenes[sceneName] != undefined)
+            SCG.scenes.selectScene(sceneName);
+        else 
+            return;
+    }
+
+    SCG.globals.version = 0.1;
+
+    SCG.src = {
+        
+    }
+
+    
+    SCG.scenes.cacheScene(new GraveyardMorningScene({
+        name:'graveyard',
+        viewport: new V2(200, 150)
+    }));
+
+    SCG.scenes.cacheScene(new GraveyardEveningScene({
+        name:'graveyard_evening',
+        viewport: new V2(200, 150)
+    }));
+
+    SCG.main.startV2('graveyard_evening');
+});
