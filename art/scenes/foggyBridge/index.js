9190caf474214fb64e2fc951d55f696d987d4aa3
Starykh Aleksey <astarikh@mail.ru>
2021-08-20 21:45:49 +0300

    new scene done

---
+++ art/scenes/foggyBridge/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function sceneSelectByHashValue(){
+        let sceneName = location.hash.replace('#','');
+        if(sceneName == ''){
+            return;
+        }
+        
+        if(SCG.scenes.cachedScenes[sceneName] != undefined)
+            SCG.scenes.selectScene(sceneName);
+        else 
+            return;
+    }
+
+    SCG.globals.version = 0.1;
+
+    SCG.src = {
+        
+    }
+
+    
+    SCG.scenes.cacheScene(new FoggyBridgeScene({
+        name:'foggy',
+        viewport: new V2(200,200)
+    }));
+
+    SCG.main.startV2('foggy');
+});
